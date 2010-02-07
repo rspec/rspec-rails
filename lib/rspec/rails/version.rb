7b8072d69bc0d83ea86e8ae9722a9b11ec18883d
David Chelimsky <dchelimsky@gmail.com>
2010-02-07 06:32:01 -0600

    bump version

--- lib/rspec/rails/version.rb
+++ lib/rspec/rails/version.rb
@@ -5,7 +5,7 @@ module Rspec # :nodoc:
         MAJOR  = 2
         MINOR  = 0
         TINY   = 0
-        PRE    = 'a4'
+        PRE    = 'a5'
 
         STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
 
