de235eb486c41f52d15cb460028ddde573bd9836
David Chelimsky <dchelimsky@gmail.com>
2010-02-07 12:01:57 -0600

    dummy example for request specs
    - also disable generation of helper specs

--- lib/generators/rspec/helper/helper_generator.rb
+++ lib/generators/rspec/helper/helper_generator.rb
@@ -3,7 +3,11 @@ require 'generators/rspec'
 module Rspec
   module Generators
     class HelperGenerator < Base
+      class_option :helpers, :type => :boolean, :default => false
+
       def create_helper_files
+        return unless options[:helpers]
+
         template 'helper_spec.rb', File.join('spec/helpers', class_path, "#{file_name}_helper_spec.rb")
       end
     end
