ed76f80641e0bec455603dbeb69c322327f1f774
David Chelimsky <dchelimsky@gmail.com>
2010-02-20 00:13:06 -0600

    fix gem version format

--- example_app_template.rb
+++ example_app_template.rb
@@ -2,7 +2,7 @@ $LOAD_PATH.unshift(File.expand_path('../../../lib', __FILE__))
 require 'rspec/rails/version'
 # This needs to be installed on the system, as well as all of its rspec-2 deps
 # - would be nicer if we could source them from the <repo>/pkg dirs
-gem 'rspec-rails', :version => "'#{Rspec::Rails::Version::STRING}'"
+gem 'rspec-rails', :version => Rspec::Rails::Version::STRING
 
 run('bundle install')
 
