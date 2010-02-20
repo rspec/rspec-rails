878d74bccd4c8fc2aaec103f46ace87b05ff1c16
David Chelimsky <dchelimsky@gmail.com>
2010-02-20 09:49:47 -0600

    align view spec templates
    - need to figure out how to use templates from
      other generators

--- lib/generators/rspec/controller/templates/view_spec.rb
+++ lib/generators/rspec/controller/templates/view_spec.rb
@@ -1,12 +1,9 @@
 require 'spec_helper'
 
 describe "/<%= file_name %>/<%= @action %>.html.<%= options[:template_engine] %>" do
-  before(:each) do
-    render '<%= file_name %>/<%= @action %>'
-  end
-
   #Delete this example and add some real ones or delete this file
   it "should tell you where to find the file" do
-    response.should have_tag('p', %r[Find me in app/views/<%= file_path %>/<%= @action %>])
+    render
+    response.should have_selector('p', :content => "Find me in app/views/<%= file_path %>/<%= @action %>")
   end
 end
