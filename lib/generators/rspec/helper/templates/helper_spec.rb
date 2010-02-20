5e979fc7c36a8fecaef14d3e100d7c4a07402ea6
David Chelimsky <dchelimsky@gmail.com>
2010-02-20 08:42:28 -0600

    fix commented example

--- lib/generators/rspec/helper/templates/helper_spec.rb
+++ lib/generators/rspec/helper/templates/helper_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 # Specs in this file have access to a helper object that includes
 # the <%= class_name %>Helper. For example:
 # 
-# describe <%= class_name %>Helper 
+# describe <%= class_name %>Helper do
 #   describe "string concat" do
 #     it "concats two strings with spaces" do
 #       helper.concat_strings("this","that").should == "this that"
