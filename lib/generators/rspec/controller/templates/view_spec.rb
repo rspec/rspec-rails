004562c3f21b57fa22d9beb1b63cfc690248f6d5
David Chelimsky <dchelimsky@gmail.com>
2010-02-21 14:20:22 -0600

    make gen'd view examples pending

--- lib/generators/rspec/controller/templates/view_spec.rb
+++ lib/generators/rspec/controller/templates/view_spec.rb
@@ -1,9 +1,5 @@
 require 'spec_helper'
 
 describe "/<%= file_name %>/<%= @action %>.html.<%= options[:template_engine] %>" do
-  #Delete this example and add some real ones or delete this file
-  it "should tell you where to find the file" do
-    render
-    response.should have_selector('p', :content => "Find me in app/views/<%= file_path %>/<%= @action %>")
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
