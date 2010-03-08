df2b06352186a087d479499ffdae6af8f1d85721
David Chelimsky <dchelimsky@gmail.com>
2010-03-08 01:52:25 -0600

    singular

--- templates/generate_stuff.rb
+++ templates/generate_stuff.rb
@@ -4,5 +4,5 @@ generate('integration_test widgets')
 generate('mailer Notifications signup')
 generate('model thing name:string')
 generate('observer widget')
-generate('scaffold widgets name:string')
+generate('scaffold widget name:string')
 
