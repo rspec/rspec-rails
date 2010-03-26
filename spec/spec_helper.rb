49c5ecafc6fe38e59642de2096a6c38033acece4
David Chelimsky <dchelimsky@gmail.com>
2010-03-26 05:44:30 -0500

    add Rspec configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,3 +12,12 @@ require 'action_dispatch'
 require 'action_controller'
 require 'active_record'
 require 'rspec/rails'
+
+def in_editor?
+  ENV.has_key?('TM_MODE') || ENV.has_key?('EMACS') || ENV.has_key?('VIM')
+end
+
+Rspec.configure do |c|
+  c.color_enabled = !in_editor?
+end
+
