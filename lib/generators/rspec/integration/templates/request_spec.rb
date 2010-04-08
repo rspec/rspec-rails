e335a44693db82ebe301b368c9daabbe071354fc
David Chelimsky <dchelimsky@gmail.com>
2010-04-07 19:20:33 -0500

    wrap in quotes, silly

--- lib/generators/rspec/integration/templates/request_spec.rb
+++ lib/generators/rspec/integration/templates/request_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe <%= class_name.pluralize %> do
+describe "<%= class_name.pluralize %>" do
   describe "GET /<%= table_name %>" do
     it "works! (now write some real specs)" do
       get <%= table_name %>_path
