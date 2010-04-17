80b47c1a965e88a599521113409c07d200bbaa3e
Iain Hecker <iain@iain.nl>
2010-04-17 17:20:46 +0200

    Use Rails module in generated files [#31 state:resolved]

--- lib/generators/rspec/install/templates/lib/tasks/rspec.rake
+++ lib/generators/rspec/install/templates/lib/tasks/rspec.rake
@@ -28,7 +28,7 @@ end
 
 Rake.application.instance_variable_get('@tasks').delete('default')
 
-spec_prereq = File.exist?(File.join(Rails.root, 'config', 'database.yml')) ? "db:test:prepare" : :noop
+spec_prereq = Rails.root.join('config', 'database.yml').exist? ? "db:test:prepare" : :noop
 task :noop do
 end
 
--- lib/generators/rspec/install/templates/spec/spec_helper.rb
+++ lib/generators/rspec/install/templates/spec/spec_helper.rb
@@ -1,7 +1,7 @@
 # This file is copied to ~/spec when you run 'ruby script/generate rspec'
 # from the project root directory.
 ENV["RAILS_ENV"] ||= 'test'
-require File.dirname(__FILE__) + "/../config/environment" unless defined?(RAILS_ROOT)
+require File.dirname(__FILE__) + "/../config/environment" unless defined?(Rails)
 require 'rspec/rails'
 
 # Requires supporting files with custom matchers and macros, etc,
