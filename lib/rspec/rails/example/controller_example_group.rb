10c66e28e9381851d8e8c6a8ad2386978aefa8c5
David Chelimsky <dchelimsky@gmail.com>
2010-04-28 06:00:14 -0500

    Add controller attr_reader to controller specs
    Closes #39

--- lib/rspec/rails/example/controller_example_group.rb
+++ lib/rspec/rails/example/controller_example_group.rb
@@ -4,17 +4,22 @@ require 'webrat'
 module ControllerExampleGroupBehaviour
   extend ActiveSupport::Concern
 
-  module ControllerClassReader
+  module ClassAttributeReaders
     def controller_class
       describes
     end
+  end
+
+  module AttributeReaders
+    attr_reader :controller
   end
 
   included do
     extend  Rspec::Rails::SetupAndTeardownAdapter
     include Rspec::Rails::TestUnitAssertionAdapter
     include ActionController::TestCase::Behavior
-    extend  ControllerClassReader
+    extend  ClassAttributeReaders
+    include AttributeReaders
     include Webrat::Matchers
     include Webrat::Methods
     include Rspec::Matchers
