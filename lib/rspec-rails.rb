1b8bdd7090992d1cd8f3a1cd2dfbb7dc1f6205ef
Carlhuda <carlhuda@engineyard.com>
2010-04-30 13:34:41 -0700

    By default, rspec-rails should work without the generated initializers

--- lib/rspec-rails.rb
+++ lib/rspec-rails.rb
@@ -1,6 +1,9 @@
 module RSpec
   module Rails
     class Railtie < ::Rails::Railtie
+      config.generators.integration_tool :rspec
+      config.generators.test_framework   :rspec
+
       rake_tasks do
         load "rspec/rails/tasks/rspec.rake"
       end
