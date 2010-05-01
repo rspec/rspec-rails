38c04ca670d97bd591e47b5b011555b684e06149
David Chelimsky <dchelimsky@gmail.com>
2010-05-01 07:50:49 -0500

    Use paths relative to rspec-rails directory in template

--- example_app_template.rb
+++ example_app_template.rb
@@ -1,5 +1,5 @@
-$LOAD_PATH.unshift(File.expand_path('../../../lib', __FILE__))
+$LOAD_PATH.unshift(File.expand_path('../lib', __FILE__))
 require 'rspec/rails/version'
 
-gem 'rspec-rails', :path => File.expand_path('../../../', __FILE__)
+gem 'rspec-rails', :path => File.expand_path('../', __FILE__)
 
