1acd9e3f4b925bd25553507d92984be96395b256
David Chelimsky <dchelimsky@gmail.com>
2010-05-01 07:49:46 -0500

    don't try to copy/include files that got removed in the last 3 commits

--- lib/generators/rspec/install/install_generator.rb
+++ lib/generators/rspec/install/install_generator.rb
@@ -11,16 +11,6 @@ DESC
         @source_root ||= File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
       end
 
-      def copy_lib_files
-        directory 'lib'
-      end
-
-      def create_config_files
-        inside "config" do
-          directory "initializers"
-        end
-      end
-
       def copy_spec_files
         directory 'spec'
       end
