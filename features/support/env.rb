21a0c1e9af314dfb4429d6d8fb6dfc8faebd4fc0
David Chelimsky <dchelimsky@gmail.com>
2010-06-05 17:21:40 -0400

    Copy the Gemfile.lock file instead of symlinking it
    - speeds rspec-rails' own cukes from 2m36s to 30s

--- features/support/env.rb
+++ features/support/env.rb
@@ -19,16 +19,26 @@ def write_symlink(file_or_dir)
   system "ln -s #{source} #{target}"
 end
 
+def copy(file_or_dir)
+  source = example_app_path(file_or_dir)
+  target = aruba_path(file_or_dir)
+  system "cp -r #{source} #{target}"
+end
+
 Before do
   steps %Q{
     Given a directory named "spec"
   }
 
   Dir['tmp/example_app/*'].each do |file_or_dir|
-    unless file_or_dir =~ /spec$/
+    if file_or_dir =~ /Gemfile/
+      copy(file_or_dir)
+    elsif !(file_or_dir =~ /spec$/)
       write_symlink(file_or_dir)
     end
   end
+
+  copy('tmp/example_app/.bundle')
 
   ["spec/spec_helper.rb"].each do |file_or_dir|
     write_symlink("tmp/example_app/#{file_or_dir}")
