d079ac11d564484ce23f22cbe4fdbcbf5520519e
David Chelimsky <dchelimsky@gmail.com>
2010-06-12 09:23:24 -0400

    generate proper mailer spec

--- lib/generators/rspec/mailer/templates/mailer_spec.rb
+++ lib/generators/rspec/mailer/templates/mailer_spec.rb
@@ -1,13 +1,23 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe <%= class_name %> do
 <% for action in actions -%>
-  it "should deliver <%= action.gsub("_", " ") %> message" do
-    @expected.subject = <%= action.to_s.humanize.inspect %>
-    @expected.to      = "to@example.org"
-    @expected.from    = "from@example.com"
-    @expected.body    = read_fixture("<%= action %>")
+  describe "<%= action %>" do
+    let(:mail) { <%= class_name %>.<%= action %> }
+
+    it "renders the headers" do
+      mail.subject.should eq(<%= action.to_s.humanize.inspect %>)
+      mail.to.should eq(["to@example.org"])
+      mail.from.should eq(["from@example.com"])
+    end
+
+    it "renders the body" do
+      mail.body.encoded.should match("Hi")
+    end
   end
 
+<% end -%>
+<% if actions.blank? -%>
+  pending "add some examples to (or delete) #{__FILE__}"
 <% end -%>
 end
