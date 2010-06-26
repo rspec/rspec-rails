0c35483431ddf4178ef5b53b729adf03e0e0f744
David Chelimsky <dchelimsky@gmail.com>
2010-06-26 12:20:06 -0500

    Fix environment loading bug in spec_helper
    - require using File.expand_path (which Rails is using as well) rather
      than using a guard (which turns out to be WRONG).
    - Thanks to José Valim for the assist

--- lib/generators/rspec/install/templates/spec/spec_helper.rb
+++ lib/generators/rspec/install/templates/spec/spec_helper.rb
@@ -1,7 +1,7 @@
 # This file is copied to ~/spec when you run 'ruby script/generate rspec'
 # from the project root directory.
 ENV["RAILS_ENV"] ||= 'test'
-require File.dirname(__FILE__) + "/../config/environment" unless defined?(Rails)
+require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
 # Requires supporting files with custom matchers and macros, etc,
