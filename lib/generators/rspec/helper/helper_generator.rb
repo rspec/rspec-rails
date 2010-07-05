779738d74c87b430fd490f19a0b15b867a2d94e0
David Chelimsky <dchelimsky@gmail.com>
2010-07-05 11:55:55 -0500

    Configure helper specs to be generated by default when invoked by Rails.
    Closes #115.

--- lib/generators/rspec/helper/helper_generator.rb
+++ lib/generators/rspec/helper/helper_generator.rb
@@ -4,12 +4,12 @@ module Rspec
   module Generators
     class HelperGenerator < Base
       include Rails::Generators::ResourceHelpers
-      class_option :helpers, :type => :boolean, :default => false
+      class_option :helpers, :type => :boolean, :default => true
 
       def create_helper_files
         return unless options[:helpers]
 
-        template 'helper_spec.rb', File.join('spec/helpers', class_path, "#{file_name}_helper_spec.rb")
+        template 'helper_spec.rb', File.join('spec/helpers', class_path, "#{table_name}_helper_spec.rb")
       end
     end
   end
