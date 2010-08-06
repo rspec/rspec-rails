59c83c6d4fb6d6d3dd6f839560232608a1aaced6
David Chelimsky <dchelimsky@gmail.com>
2010-08-05 22:05:47 -0500

    refactor the be_a_new matcher

--- lib/rspec/rails/matchers/be_a_new.rb
+++ lib/rspec/rails/matchers/be_a_new.rb
@@ -1,36 +1,40 @@
 RSpec::Matchers.define :be_a_new do |model_klass|
-  def with(attributes)
-    @attributes = attributes.stringify_keys.to_a
-    self
+  match do |actual|
+    actual.is_a?(model_klass) && actual.new_record? && attributes_match?(actual)
   end
 
-  match do |actual|
-    @unmatched_attributes = Hash.new
-
-    @attributes.each do |key, value|
-      unless actual.attributes[key].eql?(value)
-        @unmatched_attributes.merge!(key => value)
-      end
-    end if @attributes
-
-    @is_model_klass_new_record = actual.is_a?(model_klass) &&
-                                 actual.new_record?
-
-    @unmatched_attributes.empty? && @is_model_klass_new_record
+  chain :with do |expected_attributes|
+    attributes.merge!(expected_attributes)
   end
 
   failure_message_for_should do |actual|
     [].tap do |message|
-      unless @is_model_klass_new_record
+      unless actual.is_a?(model_klass) && actual.new_record? 
         message << "expected #{actual.inspect} to be a new #{model_klass.inspect}"
       end
-      unless @unmatched_attributes.empty?
-        if @unmatched_attributes.size > 1
-          message << "attributes #{@unmatched_attributes.inspect} were not set on #{actual.inspect}"
+      unless attributes_match?(actual)
+        if unmatched_attributes.size > 1
+          message << "attributes #{unmatched_attributes.inspect} were not set on #{actual.inspect}"
         else
-          message << "attribute #{@unmatched_attributes.inspect} was not set on #{actual.inspect}"
+          message << "attribute #{unmatched_attributes.inspect} was not set on #{actual.inspect}"
         end
       end
     end.join(' and ')
+  end
+
+  def attributes
+    @attributes ||= {}
+  end
+
+  def attributes_match?(actual)
+    attributes.stringify_keys.all? do |key, value|
+      actual.attributes[key].eql?(value)
+    end
+  end
+
+  def unmatched_attributes
+    attributes.stringify_keys.reject do |key, value|
+      actual.attributes[key].eql?(value)
+    end
   end
 end
