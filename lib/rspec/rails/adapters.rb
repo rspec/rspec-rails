46ab6bfec701b5a07c9b91fe1395c2af9b83ca62
David Chelimsky <dchelimsky@gmail.com>
2010-08-08 19:44:39 -0500

    Wrap T/U and/or MiniTest assertions in a separate scope from that of the example.
    - Closes #152.

--- lib/rspec/rails/adapters.rb
+++ lib/rspec/rails/adapters.rb
@@ -15,20 +15,46 @@ module RSpec
           methods.each {|method| after { send method } }
         end
       end
+
+      module InstanceMethods
+        def method_name
+          @example
+        end
+      end
     end
 
     module TestUnitAssertionAdapter
       extend ActiveSupport::Concern
-      def method_name
-        @example
+
+      class AssertionDelegate
+        include Test::Unit::Assertions
       end
 
-      include Test::Unit::Assertions
+      module ClassMethods
+        def assertion_method_names
+          Test::Unit::Assertions.public_instance_methods.select{|m| m.to_s =~ /^assert/} +
+            [:build_message]
+        end
+
+        def define_assertion_delegators
+          assertion_method_names.each do |m|
+            class_eval <<-CODE
+              def #{m}(*args, &block)
+                assertion_delegate.send :#{m}, *args, &block
+              end
+            CODE
+          end
+        end
+      end
+
+      module InstanceMethods
+        def assertion_delegate
+          @assertion_delegate ||= AssertionDelegate.new
+        end
+      end
 
       included do
-        before do
-          @_result = Struct.new(:add_assertion).new
-        end
+        define_assertion_delegators
       end
     end
   end
