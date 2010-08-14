72ecc0351500d25c73bdc601e4e730b323293337
justinko <jko170@gmail.com>
2010-08-14 15:59:45 -0500

    Remove 'should' from example docstrings.
    - Closes #162.

--- spec/rspec/rails/matchers/errors_on_spec.rb
+++ spec/rspec/rails/matchers/errors_on_spec.rb
@@ -1,11 +1,11 @@
 require "spec_helper"
 
 describe "error_on" do
-  it "should provide a description including the name of what the error is on" do
+  it "provides a description including the name of what the error is on" do
     have(1).error_on(:whatever).description.should == "have 1 error on :whatever"
   end
 
-  it "should provide a failure message including the number actually given" do
+  it "provides a failure message including the number actually given" do
     lambda {
       [].should have(1).error_on(:whatever)
     }.should raise_error("expected 1 error on :whatever, got 0")
@@ -13,11 +13,11 @@ describe "error_on" do
 end
 
 describe "errors_on" do
-  it "should provide a description including the name of what the error is on" do
+  it "provides a description including the name of what the error is on" do
     have(2).errors_on(:whatever).description.should == "have 2 errors on :whatever"
   end
 
-  it "should provide a failure message including the number actually given" do
+  it "provides a failure message including the number actually given" do
     lambda {
       [1].should have(3).errors_on(:whatever)
     }.should raise_error("expected 3 errors on :whatever, got 1")
