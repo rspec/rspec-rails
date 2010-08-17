c7ae905daa0b520d6de45ca4bbf1a26820de1810
Andre Arko <andre@arko.net>
2010-08-16 23:49:41 -0700

    Clean up spec_helper a bit for Rails 3

--- lib/generators/rspec/install/templates/spec/spec_helper.rb
+++ lib/generators/rspec/install/templates/spec/spec_helper.rb
@@ -1,12 +1,11 @@
-# This file is copied to ~/spec when you run 'ruby script/generate rspec'
-# from the project root directory.
+# This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
   # == Mock Framework
