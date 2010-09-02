8f6a42f5311539ded0af10d30fc5810113029e3d
David Chelimsky <dchelimsky@gmail.com>
2010-09-02 00:30:01 -0500

    add comment to remove fixture path from spec helper if you're not using AR

--- lib/generators/rspec/install/templates/spec/spec_helper.rb
+++ lib/generators/rspec/install/templates/spec/spec_helper.rb
@@ -17,10 +17,11 @@ RSpec.configure do |config|
   # config.mock_with :rr
   config.mock_with :rspec
 
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, comment the following line or assign false
+  # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
 end
