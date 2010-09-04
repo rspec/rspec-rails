b4cc9d83643dfb5bb8fd97fe2cb0fb4f4246c1cc
Lailson Bandeira <lailson@guava.com.br>
2010-09-04 19:06:00 -0300

    Removed conditional around ActiveRecord::TestFixtures inclusion.
    Actually, I cannot figure out why it was there.

--- lib/rspec/rails/fixture_support.rb
+++ lib/rspec/rails/fixture_support.rb
@@ -8,15 +8,13 @@ if defined?(ActiveRecord)
         include RSpec::Rails::TestUnitAssertionAdapter
 
         included do
-          if RSpec.configuration.use_transactional_fixtures
-            # TODO - figure out how to move this outside the included block
-            include ActiveRecord::TestFixtures
+          # TODO - figure out how to move this outside the included block
+          include ActiveRecord::TestFixtures
 
-            self.fixture_path = RSpec.configuration.fixture_path
-            self.use_transactional_fixtures = RSpec.configuration.use_transactional_fixtures
-            self.use_instantiated_fixtures  = RSpec.configuration.use_instantiated_fixtures
-            fixtures RSpec.configuration.global_fixtures if RSpec.configuration.global_fixtures
-          end
+          self.fixture_path = RSpec.configuration.fixture_path
+          self.use_transactional_fixtures = RSpec.configuration.use_transactional_fixtures
+          self.use_instantiated_fixtures  = RSpec.configuration.use_instantiated_fixtures
+          fixtures RSpec.configuration.global_fixtures if RSpec.configuration.global_fixtures
         end
       end
     end
