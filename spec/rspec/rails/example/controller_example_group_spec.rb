ca16b51d9afe584b5ce5b4e77898cd74356f5783
David Chelimsky <dchelimsky@gmail.com>
2010-09-30 00:07:12 -0500

    refactor spec

--- spec/rspec/rails/example/controller_example_group_spec.rb
+++ spec/rspec/rails/example/controller_example_group_spec.rb
@@ -5,44 +5,34 @@ module RSpec::Rails
     it { should be_included_in_files_in('./spec/controllers/') }
     it { should be_included_in_files_in('.\\spec\\controllers\\') }
 
-    it "includes routing matchers" do
-      group = RSpec::Core::ExampleGroup.describe do
+    let(:group) do
+      RSpec::Core::ExampleGroup.describe do
         include ControllerExampleGroup
       end
+    end
+
+    it "includes routing matchers" do
       group.included_modules.should include(RSpec::Rails::Matchers::RoutingMatchers)
     end
 
     it "adds :type => :controller to the metadata" do
-      group = RSpec::Core::ExampleGroup.describe do
-        include ControllerExampleGroup
-      end
       group.metadata[:type].should eq(:controller)
     end
 
-    it "should use the controller as the implicit subject" do
-      group = RSpec::Core::ExampleGroup.describe do
-        include ControllerExampleGroup
+    context "with implicit subject" do
+      it "uses the controller as the subject" do
+        controller = double('controller')
+        example = group.new
+        example.stub(:controller => controller)
+        example.subject.should == controller
       end
-
-      example = group.new
-
-      controller = double('controller')
-      example.stub(:controller => controller)
-
-      example.subject.should == controller
     end
 
-    describe "with a specified subject" do
-      before do
-        @group = RSpec::Core::ExampleGroup.describe do
-          include ControllerExampleGroup
-          subject { 'specified' }
-        end
-      end
-
+    describe "with explicit subject" do
       it "should use the specified subject instead of the controller" do
-        example = @group.new
-        example.subject.should == 'specified'
+        group.subject { 'explicit' }
+        example = group.new
+        example.subject.should == 'explicit'
       end
     end
   end
