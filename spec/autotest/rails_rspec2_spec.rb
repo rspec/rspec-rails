a98567657436902d4bdc96f689e24f84cd504e9a
Matthew Todd <matthew.todd@gmail.com>
2010-10-08 15:28:30 +0300

    Make Autotest::RailsRspec2 honor spec/support/*.rb
    Previously, it looked at a subdirectory called "shared," but the
    generated spec_helper guides us towards one named "support" instead.
    
    (Alternatively, we could have changed the generated spec_helper to
    suggest "shared" instead, but since cucumber uses "features/support,"
    this seems like a good choice.)

--- spec/autotest/rails_rspec2_spec.rb
+++ spec/autotest/rails_rspec2_spec.rb
@@ -2,24 +2,43 @@ require "spec_helper"
 require "autotest/rails_rspec2"
 
 describe Autotest::RailsRspec2 do
-  before(:each) do
-    rails_rspec2_autotest = Autotest::RailsRspec2.new
-    @re = rails_rspec2_autotest.exceptions
+  before do
+    @rails_rspec2_autotest = Autotest::RailsRspec2.new
   end
 
-  it "should match './log/test.log'" do
-    @re.should match('./log/test.log')
+  describe 'exceptions' do
+    before do
+      @re = @rails_rspec2_autotest.exceptions
+    end
+
+    it "should match './log/test.log'" do
+      @re.should match('./log/test.log')
+    end
+
+    it "should match 'log/test.log'" do
+      @re.should match('log/test.log')
+    end
+
+    it "should not match './spec/models/user_spec.rb'" do
+      @re.should_not match('./spec/models/user_spec.rb')
+    end
+
+    it "should not match 'spec/models/user_spec.rb'" do
+      @re.should_not match('spec/models/user_spec.rb')
+    end
   end
 
-  it "should match 'log/test.log'" do
-    @re.should match('log/test.log')
-  end
+  describe 'mappings' do
+    before do
+      @rails_rspec2_autotest.find_order = %w(
+        spec/models/user_spec.rb
+        spec/support/blueprints.rb
+      )
+    end
 
-  it "should not match './spec/models/user_spec.rb'" do
-    @re.should_not match('./spec/models/user_spec.rb')
-  end
-
-  it "should not match 'spec/models/user_spec.rb'" do
-    @re.should_not match('spec/models/user_spec.rb')
+    it 'runs model specs when support files change' do
+      @rails_rspec2_autotest.test_files_for('spec/support/blueprints.rb').should(
+        include('spec/models/user_spec.rb'))
+    end
   end
 end
