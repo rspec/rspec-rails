dd0095af9088a00f1a4d1717c137bc79e365a6ab
Emanuele Vicentini <emanuele.vicentini@gmail.com>
2010-10-15 09:19:16 +0200

    Tweaked assign() to work with Ruby on Rails 3.0.1.
    - Closes #243.

--- lib/rspec/rails/view_assigns.rb
+++ lib/rspec/rails/view_assigns.rb
@@ -13,14 +13,14 @@ module RSpec
           _encapsulated_assigns[key] = value
         end
 
-        if ::Rails::VERSION::STRING == "3.0.0"
+        if ::Rails::VERSION::STRING =~ /3\.0\.[01]/
           def _assigns
             super.merge(_encapsulated_assigns)
           end
           def view_assigns
             _assigns
           end
-        else # >= 3.0.1
+        else # >= 3.0.2 maybe...
           def view_assigns
             super.merge(_encapsulated_assigns)
           end
