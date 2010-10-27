ab6dbea76fba47052e9bc22b6d8ffd85114ef501
David Chelimsky <dchelimsky@gmail.com>
2010-10-27 19:05:09 -0200

    Move error[s]_on to ActiveModel::Validations so it can be used by other ActiveModel compliant ORMs
    - Closes ????

--- lib/rspec/rails/extensions/active_record/base.rb
+++ lib/rspec/rails/extensions/active_record/base.rb
@@ -3,42 +3,38 @@ module RSpec
     if using_active_record?
       module Extensions
         module ActiveRecord
-          module ClassMethods
-            # :call-seq:
-            #   ModelClass.should have(:no).records
-            #   ModelClass.should have(1).record
-            #   ModelClass.should have(n).records
-            #
-            # Extension to enhance <tt>should have</tt> on AR Model classes
-            def records
-              find(:all)
-            end
-            alias :record :records
+          # :call-seq:
+          #   ModelClass.should have(:no).records
+          #   ModelClass.should have(1).record
+          #   ModelClass.should have(n).records
+          #
+          # Extension to enhance <tt>should have</tt> on AR Model classes
+          def records
+            find(:all)
           end
-
-          module InstanceMethods
-            # :call-seq:
-            #   model.should have(:no).errors_on(:attribute)
-            #   model.should have(1).error_on(:attribute)
-            #   model.should have(n).errors_on(:attribute)
-            #
-            # Extension to enhance <tt>should have</tt> on AR Model instances.
-            # Calls model.valid? in order to prepare the object's errors
-            # object.
-            def errors_on(attribute)
-              self.valid?
-              [self.errors[attribute]].flatten.compact
-            end
-            alias :error_on :errors_on
-          end
+          alias :record :records
         end
-      end
 
-      class ::ActiveRecord::Base #:nodoc:
-        extend  RSpec::Rails::Extensions::ActiveRecord::ClassMethods
-        include RSpec::Rails::Extensions::ActiveRecord::InstanceMethods
+        class ::ActiveRecord::Base #:nodoc:
+          extend RSpec::Rails::Extensions::ActiveRecord
+        end
       end
     end
   end
 end
 
+module ::ActiveModel::Validations
+  # :call-seq:
+  #   model.should have(:no).errors_on(:attribute)
+  #   model.should have(1).error_on(:attribute)
+  #   model.should have(n).errors_on(:attribute)
+  #
+  # Extension to enhance <tt>should have</tt> on AR Model instances.
+  # Calls model.valid? in order to prepare the object's errors
+  # object.
+  def errors_on(attribute)
+    self.valid?
+    [self.errors[attribute]].flatten.compact
+  end
+  alias :error_on :errors_on
+end
