b92acde12cec191fb37fb0aa02a9dab3c83ca2be
David Chelimsky <dchelimsky@gmail.com>
2010-12-12 09:15:26 -0600

    the final frontier

--- templates/generate_stuff.rb
+++ templates/generate_stuff.rb
@@ -7,7 +7,7 @@ generate('model thing name:string')
 generate('helper things')
 generate('scaffold widget name:string category:string instock:boolean')
 generate('observer widget')
-generate('scaffold gadget ') # scaffold with no attributes
+generate('scaffold gadget') # scaffold with no attributes
 
 run('rake db:migrate')
 run('rake db:test:prepare')
