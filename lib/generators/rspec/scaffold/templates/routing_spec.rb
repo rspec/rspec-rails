ccd00094de1988e84c22d765e555e2e826c6cdd3
David Chelimsky <dchelimsky@gmail.com>
2011-03-05 11:08:00 -0600

    use shortcuts in generated routing specs

--- lib/generators/rspec/scaffold/templates/routing_spec.rb
+++ lib/generators/rspec/scaffold/templates/routing_spec.rb
@@ -5,32 +5,32 @@ describe <%= controller_class_name %>Controller do
 
 <% unless options[:singleton] -%>
     it "recognizes and generates #index" do
-      { :get => "/<%= table_name %>" }.should route_to(:controller => "<%= table_name %>", :action => "index")
+      get("/<%= table_name %>").should route_to("<%= table_name %>#index")
     end
 
 <% end -%>
     it "recognizes and generates #new" do
-      { :get => "/<%= table_name %>/new" }.should route_to(:controller => "<%= table_name %>", :action => "new")
+      get("/<%= table_name %>/new").should route_to("<%= table_name %>#new")
     end
 
     it "recognizes and generates #show" do
-      { :get => "/<%= table_name %>/1" }.should route_to(:controller => "<%= table_name %>", :action => "show", :id => "1")
+      get("/<%= table_name %>/1").should route_to("<%= table_name %>#show", :id => "1")
     end
 
     it "recognizes and generates #edit" do
-      { :get => "/<%= table_name %>/1/edit" }.should route_to(:controller => "<%= table_name %>", :action => "edit", :id => "1")
+      get("/<%= table_name %>/1/edit").should route_to("<%= table_name %>#edit", :id => "1")
     end
 
     it "recognizes and generates #create" do
-      { :post => "/<%= table_name %>" }.should route_to(:controller => "<%= table_name %>", :action => "create")
+      post("/<%= table_name %>").should route_to("<%= table_name %>#create")
     end
 
     it "recognizes and generates #update" do
-      { :put => "/<%= table_name %>/1" }.should route_to(:controller => "<%= table_name %>", :action => "update", :id => "1")
+      put("/<%= table_name %>/1").should route_to("<%= table_name %>#update", :id => "1")
     end
 
     it "recognizes and generates #destroy" do
-      { :delete => "/<%= table_name %>/1" }.should route_to(:controller => "<%= table_name %>", :action => "destroy", :id => "1")
+      delete("/<%= table_name %>/1").should route_to("<%= table_name %>#destroy", :id => "1")
     end
 
   end
