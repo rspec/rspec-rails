792afc323069b52955e8790a9db2505c95681445
David Chelimsky <dchelimsky@gmail.com>
2011-04-24 16:11:26 -0500

    exclude dev-only gems for builds on travis

--- gemfiles/base.rb
+++ gemfiles/base.rb
@@ -17,24 +17,34 @@ module GemfileBase
       gem 'sqlite3-ruby', :require => 'sqlite3'
       gem "cucumber", "~> 0.10.2"
       gem "aruba", "~> 0.3.6"
-      gem "rcov", "0.9.9"
-      gem "relish", "0.2.0"
-      gem "guard-rspec", "0.1.9"
       gem "growl", "1.0.3"
       gem "ZenTest", "~> 4.4.2"
       gem "webrat", "0.7.2"
 
-      if RUBY_PLATFORM =~ /darwin/
-        gem "autotest-fsevent", "~> 0.2.4"
-        gem "autotest-growl", "~> 0.2.9"
-      end
+      unless ENV['CI']
+        gem "rcov", "0.9.9"
+        gem "relish", "0.2.0"
+        gem "guard-rspec", "0.1.9"
 
-      gem "ruby-debug", :platforms => :ruby_18
-      gem "ruby-debug19", "~> 0.11.6", :platforms => :ruby_19
+        if RUBY_PLATFORM =~ /darwin/
+          gem "autotest-fsevent", "~> 0.2.4"
+          gem "autotest-growl", "~> 0.2.9"
+        end
 
-      platforms :ruby_18, :ruby_19 do
-        gem "rb-fsevent", "~> 0.3.9"
-        gem "ruby-prof", "~> 0.9.2"
+        platforms :mri_18 do
+          gem 'ruby-debug'
+        end
+
+        platforms :mri_19 do
+          gem 'linecache19', '0.5.11' # 0.5.12 cannot install on 1.9.1, and 0.5.11 appears to work with both 1.9.1 & 1.9.2
+          gem 'ruby-debug19'
+          gem 'ruby-debug-base19', RUBY_VERSION == '1.9.1' ? '0.11.23' : '~> 0.11.24'
+        end
+
+        platforms :ruby_18, :ruby_19 do
+          gem "rb-fsevent", "~> 0.3.9"
+          gem "ruby-prof", "~> 0.9.2"
+        end
       end
 
       platforms :jruby do
