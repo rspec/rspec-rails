92bad323402ffb835934b9319e6006805350034d
David Chelimsky <dchelimsky@gmail.com>
2011-05-04 06:04:25 -0500

    move webrat and capybara configs to vendor

--- lib/rspec/rails/vendor/webrat.rb
+++ lib/rspec/rails/vendor/webrat.rb
@@ -1,37 +1,9 @@
 begin
-  require 'capybara/rspec'
-rescue LoadError
-end
-
-begin
-  require 'capybara/rails'
-rescue LoadError
-end
-
-begin
   require 'webrat'
 rescue LoadError
 end
 
 RSpec.configure do |c|
-  if defined?(Capybara::RSpecMatchers)
-    c.include Capybara::RSpecMatchers, :type => :view
-    c.include Capybara::RSpecMatchers, :type => :helper
-    c.include Capybara::RSpecMatchers, :type => :mailer
-    c.include Capybara::RSpecMatchers, :type => :controller
-  end
-
-  if defined?(Capybara::DSL)
-    c.include Capybara::DSL, :type => :controller
-  end
-
-  unless defined?(Capybara::RSpecMatchers) || defined?(Capybara::DSL)
-    if defined?(Capybara)
-      c.include Capybara, :type => :request
-      c.include Capybara, :type => :controller
-    end
-  end
-
   if defined?(Webrat)
     c.include Webrat::Matchers, :type => :request
     c.include Webrat::Matchers, :type => :controller
