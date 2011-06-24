327eecd6140c43efdda0c003ba316e3550edb115
Justin Ko <jko170@gmail.com>
2011-06-23 19:23:01 -0600

    should-less

--- lib/generators/rspec/controller/templates/controller_spec.rb
+++ lib/generators/rspec/controller/templates/controller_spec.rb
@@ -4,7 +4,7 @@ describe <%= class_name %>Controller do
 
 <% for action in actions -%>
   describe "GET '<%= action %>'" do
-    it "should be successful" do
+    it "is successful" do
       get '<%= action %>'
       response.should be_success
     end
