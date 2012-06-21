ab6a23d2b34b90bc1c3b486197e8c00e4c9cb42b
Ben Morris <ben@bnmrrs.com>
2012-06-21 16:57:31 -0300

    Removed the extra spaces between methods. They have been mocking me for months.

--- lib/generators/rspec/scaffold/templates/controller_spec.rb
+++ lib/generators/rspec/scaffold/templates/controller_spec.rb
@@ -26,7 +26,7 @@ describe <%= controller_class_name %>Controller do
   def valid_attributes
     {}
   end
-  
+
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
   # <%= controller_class_name %>Controller. Be sure to keep this updated too.
