1f58ed92258ad2bf10b0b8609a7bedb85d5b8823
Justin Ko <jko170@gmail.com>
2012-06-23 12:52:48 -0600

    Don't need a file for this.

--- lib/rspec/rails.rb
+++ lib/rspec/rails.rb
@@ -1,14 +1,21 @@
 require 'rspec/core'
 
-RSpec.configure do |c|
-  c.backtrace_clean_patterns << /vendor\//
-  c.backtrace_clean_patterns << /lib\/rspec\/rails/
-end
-
 module RSpec
   module Rails
     def self.at_least_rails_3_1?
       Gem::Version.new(::Rails.version) >= Gem::Version.new('3.1.0')
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.backtrace_clean_patterns << /vendor\//
+  config.backtrace_clean_patterns << /lib\/rspec\/rails/
+  config.add_setting :application, :default => ::Rails.application
+
+  unless RSpec::Rails.at_least_rails_3_1?
+    def config.application=(*)
+      raise 'Setting the application is only supported on Rails 3.1 and above.'
     end
   end
 end
@@ -23,4 +30,3 @@ require 'rspec/rails/module_inclusion'
 require 'rspec/rails/example'
 require 'rspec/rails/vendor/capybara'
 require 'rspec/rails/vendor/webrat'
-require 'rspec/rails/application'
