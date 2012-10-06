0c2b2d9626b6aa36b17a0c57f461244ec5698e46
David Chelimsky <dchelimsky@gmail.com>
2012-10-06 08:37:41 -0500

    include Capybara::DSL and Capybara::RSpecMatchers in spec/features
    - See https://github.com/jnicklas/capybara/pull/809

--- lib/rspec/rails/vendor/capybara.rb
+++ lib/rspec/rails/vendor/capybara.rb
@@ -8,20 +8,26 @@ begin
 rescue LoadError
 end
 
-RSpec.configure do |c|
-  if defined?(Capybara::RSpecMatchers)
-    c.include Capybara::RSpecMatchers, :type => :view
-    c.include Capybara::RSpecMatchers, :type => :helper
-    c.include Capybara::RSpecMatchers, :type => :mailer
-    c.include Capybara::RSpecMatchers, :type => :controller
-  end
+if defined?(Capybara)
+  RSpec.configure do |c|
+    if defined?(Capybara::DSL)
+      c.include Capybara::DSL, :type => :controller
+      c.include Capybara::DSL, :example_group => {
+        :file_path => c.escaped_path(%w[spec features])
+      }
+    end
 
-  if defined?(Capybara::DSL)
-    c.include Capybara::DSL, :type => :controller
-  end
+    if defined?(Capybara::RSpecMatchers)
+      c.include Capybara::RSpecMatchers, :type => :view
+      c.include Capybara::RSpecMatchers, :type => :helper
+      c.include Capybara::RSpecMatchers, :type => :mailer
+      c.include Capybara::RSpecMatchers, :type => :controller
+      c.include Capybara::RSpecMatchers, :example_group => {
+        :file_path => c.escaped_path(%w[spec features])
+      }
+    end
 
-  unless defined?(Capybara::RSpecMatchers) || defined?(Capybara::DSL)
-    if defined?(Capybara)
+    unless defined?(Capybara::RSpecMatchers) || defined?(Capybara::DSL)
       c.include Capybara, :type => :request
       c.include Capybara, :type => :controller
     end
