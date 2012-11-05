0fda98b77b43d785e095b6f525840f7d233d9a1d
Jonas Nicklas <jonas.nicklas@gmail.com>
2012-11-05 14:59:26 +0100

    Add example group for features
    This fixes various issues with the integration between RSpec and Capybara 2.0.
    
    - It includes Route helpers, these were previously unavailable, due to
      the fact that the only reason they are available in request spec is
      that those delegate via method_missing to an IntegrationTest instance.
    
    - It adds the `:feature` type to all features. This way people can
      include module into `:type => :feature` examples and they will work
      when their specs are places in this folder.
    
    David previously commented that he didn't want to add Capybara specific
    behaviour outside of `rspec/rails/capybara`. I was careful to avoid
    doing that. The feature example group does not explicitly include
    Capybara, instead, Capybara is included due to Capybara itself hooking
    into `:type => :feature`. This is much cleaner than the way it was
    before.
    
    I tested this against both a freshly generated Rails app, as well as an
    existing, rather large project, both worked well.

---
+++ spec/rspec/rails/example/feature_example_group_spec.rb
@@ -0,0 +1,56 @@
+require "spec_helper"
+
+module RSpec::Rails
+  describe FeatureExampleGroup do
+    it { should be_included_in_files_in('./spec/features/') }
+    it { should be_included_in_files_in('.\\spec\\features\\') }
+
+    it "adds :type => :model to the metadata" do
+      group = RSpec::Core::ExampleGroup.describe do
+        include FeatureExampleGroup
+      end
+
+      expect(group.metadata[:type]).to eql(:feature)
+    end
+
+    it "includes Rails route helpers" do
+      Rails.application.routes.draw do
+        match "/foo", :as => :foo, :to => "foo#bar"
+      end
+
+      group = RSpec::Core::ExampleGroup.describe do
+        include FeatureExampleGroup
+      end
+
+      expect(group.new.foo_path).to eql("/foo")
+      expect(group.new.foo_url).to eql("http://www.example.com/foo")
+    end
+
+    describe "#visit" do
+      it "raises an error informing about missing Capybara" do
+        group = RSpec::Core::ExampleGroup.describe do
+          include FeatureExampleGroup
+        end
+
+        expect {
+          group.new.visit('/foobar')
+        }.to raise_error(/Capybara not loaded/)
+      end
+
+      it "is resistant to load order errors" do
+        capybara = Module.new do
+          def visit(url)
+            "success: #{url}"
+          end
+        end
+
+        group = RSpec::Core::ExampleGroup.describe do
+          include capybara
+          include FeatureExampleGroup
+        end
+
+        expect(group.new.visit("/foo")).to eql("success: /foo")
+      end
+    end
+  end
+end
