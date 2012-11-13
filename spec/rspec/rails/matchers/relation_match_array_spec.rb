1d9514f41c405d25c28e01f9cf3891fb56e4be63
Andy Lindeman <alindeman@gmail.com>
2012-11-12 21:29:42 -0500

    Updates cucumber feature for changes to ActiveRecord in Rails 4
    * Rails 4 deprecates #scoped in favor of #all

--- spec/rspec/rails/matchers/relation_match_array_spec.rb
+++ spec/rspec/rails/matchers/relation_match_array_spec.rb
@@ -2,16 +2,27 @@ require "spec_helper"
 
 describe "ActiveSupport::Relation =~ matcher" do
   before { MockableModel.delete_all }
-  
+
   let!(:models) { Array.new(3) { MockableModel.create } }
 
-  it "verifies that the scope returns the records on the right hand side, regardless of order" do
-    MockableModel.scoped.should =~ models.reverse
-  end
+  if Rails.version >= '4'
+    it "verifies that the scope returns the records on the right hand side, regardless of order" do
+      MockableModel.all.should =~ models.reverse
+    end
 
-  it "fails if the scope encompasses more records than on the right hand side" do
-    MockableModel.create
-    MockableModel.scoped.should_not =~ models.reverse
+    it "fails if the scope encompasses more records than on the right hand side" do
+      MockableModel.create
+      MockableModel.all.should_not =~ models.reverse
+    end
+  else
+    it "verifies that the scope returns the records on the right hand side, regardless of order" do
+      MockableModel.scoped.should =~ models.reverse
+    end
+
+    it "fails if the scope encompasses more records than on the right hand side" do
+      MockableModel.create
+      MockableModel.scoped.should_not =~ models.reverse
+    end
   end
 
   it "fails if the scope encompasses fewer records than on the right hand side" do
