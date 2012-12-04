7ff9568b7354d396effc537b70b1eed4a06510d0
Andy Lindeman <alindeman@gmail.com>
2012-12-03 23:06:00 -0500

    Supports Rails 4 where each model has its own Relation subclass

--- lib/rspec/rails/matchers/relation_match_array.rb
+++ lib/rspec/rails/matchers/relation_match_array.rb
@@ -1,3 +1,15 @@
 if defined?(ActiveRecord::Relation)
   RSpec::Matchers::OperatorMatcher.register(ActiveRecord::Relation, '=~', RSpec::Matchers::BuiltIn::MatchArray)
+
+  module RSpec
+    module Rails
+      module RelationOperationMatcherHook
+        def inherited(subclass)
+          RSpec::Matchers::OperatorMatcher.register(subclass, '=~', RSpec::Matchers::BuiltIn::MatchArray)
+          super
+        end
+      end
+    end
+  end
+  ActiveRecord::Relation.extend(RSpec::Rails::RelationOperationMatcherHook)
 end
