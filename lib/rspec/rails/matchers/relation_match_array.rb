03b069fe5c2a278500d150e688695a77b3075180
Andy Lindeman <alindeman@gmail.com>
2012-12-05 21:38:09 -0500

    Revert "Supports Rails 4 where each model has its own Relation subclass"
    This reverts commit 3b28ce7aa0f12a24616007827112e1b5ca85eea3.
    
    * OperatorRegistry now matches on ancestor chain, so this change is no
      longer necessary. See
      https://github.com/rspec/rspec-expectations/commit/e5e5a7475cf1f993b9e1bac8df485df29a544d95.

--- lib/rspec/rails/matchers/relation_match_array.rb
+++ lib/rspec/rails/matchers/relation_match_array.rb
@@ -1,15 +1,3 @@
 if defined?(ActiveRecord::Relation)
   RSpec::Matchers::OperatorMatcher.register(ActiveRecord::Relation, '=~', RSpec::Matchers::BuiltIn::MatchArray)
-
-  module RSpec
-    module Rails
-      module RelationOperationMatcherHook
-        def inherited(subclass)
-          RSpec::Matchers::OperatorMatcher.register(subclass, '=~', RSpec::Matchers::BuiltIn::MatchArray)
-          super
-        end
-      end
-    end
-  end
-  ActiveRecord::Relation.extend(RSpec::Rails::RelationOperationMatcherHook)
 end
