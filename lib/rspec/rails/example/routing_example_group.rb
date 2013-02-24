fd374e07d7b0a9d336b569fa703b95fc29b98d4f
Andy Lindeman <alindeman@gmail.com>
2013-02-24 09:59:33 -0500

    Allows routing specs to specify their routes
    * For example, when testing Rails engines
    
    [Closes #668]

--- lib/rspec/rails/example/routing_example_group.rb
+++ lib/rspec/rails/example/routing_example_group.rb
@@ -8,16 +8,42 @@ module RSpec::Rails
     include RSpec::Rails::Matchers::RoutingMatchers::RouteHelpers
     include RSpec::Rails::AssertionDelegator.new(ActionDispatch::Assertions::RoutingAssertions)
 
+    module ClassMethods
+      # Specifies the routeset that will be used for the example group. This
+      # is most useful when testing Rails engines.
+      #
+      # @example
+      #
+      #     describe MyEngine::PostsController do
+      #       routes { MyEngine::Engine.routes }
+      #
+      #       it "routes posts#index" do
+      #         expect(:get => "/posts").to
+      #           route_to(:controller => "my_engine/posts", :action => "index")
+      #       end
+      #     end
+      def routes(&blk)
+        before do
+          self.routes = blk.call
+        end
+      end
+    end
+
     included do
       metadata[:type] = :routing
 
       before do
-        @routes = ::Rails.application.routes
-        assertion_instance.instance_variable_set(:@routes, @routes)
+        self.routes = ::Rails.application.routes
       end
     end
 
     attr_reader :routes
+
+    # @api private
+    def routes=(routes)
+      @routes = routes
+      assertion_instance.instance_variable_set(:@routes, routes)
+    end
 
     private
 
