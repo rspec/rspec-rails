6e263d8eb72641d862cc1bf5940e9d364ddbc530
Andy Lindeman <alindeman@gmail.com>
2013-04-06 13:23:26 -0400

    Extracts version checking and adds specs
    [Closes #727]

--- lib/generators/rspec/scaffold/templates/controller_spec.rb
+++ lib/generators/rspec/scaffold/templates/controller_spec.rb
@@ -113,7 +113,7 @@ describe <%= controller_class_name %>Controller do
         # specifies that the <%= class_name %> created on the previous line
         # receives the :update_attributes message with whatever params are
         # submitted in the request.
-        <%- if RSpec::Rails::Version.rails_version?('>= 4.0.0.beta1') -%>
+        <%- if RSpec::Rails.rails_version_satisfied_by?('>= 4.0.0.beta1') -%>
         <%= class_name %>.any_instance.should_receive(:update).with(<%= formatted_hash(example_params_for_update) %>)
         <%- else -%>
         <%= class_name %>.any_instance.should_receive(:update_attributes).with(<%= formatted_hash(example_params_for_update) %>)
--- lib/rspec/rails/version.rb
+++ lib/rspec/rails/version.rb
@@ -2,15 +2,6 @@ module RSpec
   module Rails
     module Version
       STRING = '2.13.0'
-
-      def self.rails_version?(version)
-        current_version = if ::Rails.version.is_a?(Gem::Version)
-                            ::Rails.version
-                          else
-                            Gem::Version.new(::Rails.version.to_s)
-                          end
-        Gem::Requirement.new(version).satisfied_by?(current_version)
-      end
     end
   end
 end
