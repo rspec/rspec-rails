b7947087eb0f74db826b6b6887c3ae209840cd97
Andy Lindeman <alindeman@gmail.com>
2013-04-06 18:24:45 -0400

    Fixtures are supported in Rails 4

--- lib/rspec/rails/example/rails_example_group.rb
+++ lib/rspec/rails/example/rails_example_group.rb
@@ -1,12 +1,14 @@
 # Temporary workaround to resolve circular dependency between rspec-rails' spec
 # suite and ammeter.
 require 'rspec/rails/matchers'
+require 'rspec/rails/rails_version'
 
 module RSpec
   module Rails
     module RailsExampleGroup
       extend ActiveSupport::Concern
       include RSpec::Rails::SetupAndTeardownAdapter
+      include RSpec::Rails::MiniTestLifecycleHooks if RSpec::Rails.rails_version_satisfied_by?('>= 4.0.0.beta1')
       include RSpec::Rails::TestUnitAssertionAdapter
       include RSpec::Rails::Matchers
     end
--- lib/rspec/rails/fixture_support.rb
+++ lib/rspec/rails/fixture_support.rb
@@ -4,6 +4,7 @@ module RSpec
       module FixtureSupport
         extend ActiveSupport::Concern
         include RSpec::Rails::SetupAndTeardownAdapter
+        include RSpec::Rails::MiniTestLifecycleHooks if RSpec::Rails.rails_version_satisfied_by?('>= 4.0.0.beta1')
         include RSpec::Rails::TestUnitAssertionAdapter
         include ActiveRecord::TestFixtures
 
