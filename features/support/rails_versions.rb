384001b0a35f1c1e5e9919229e4ffa2282739c24
Andy Lindeman <alindeman@gmail.com>
2013-04-16 23:23:23 -0400

    Reliably skip cucumber features only on Rails 3.0

--- features/support/rails_versions.rb
+++ features/support/rails_versions.rb
@@ -1,4 +1,4 @@
 Around "@unsupported-on-rails-3-0" do |scenario, block|
   require 'rails'
-  scenario.skip_invoke! if ::Rails.version.to_s.start_with?("3.0")
+  block.call unless ::Rails.version.to_s.start_with?("3.0")
 end
