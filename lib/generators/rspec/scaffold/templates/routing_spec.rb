c1814bbedc61c7ad9d41953446cfde203ff70178
Paavo Leinonen <paavo@leinonen.net>
2013-07-19 18:38:35 +0200

    Update routing spec generator to use 'expect' syntax

--- lib/generators/rspec/scaffold/templates/routing_spec.rb
+++ lib/generators/rspec/scaffold/templates/routing_spec.rb
@@ -6,32 +6,32 @@ describe <%= controller_class_name %>Controller do
 
 <% unless options[:singleton] -%>
     it "routes to #index" do
-      get("/<%= ns_table_name %>").should route_to("<%= ns_table_name %>#index")
+      expect(:get => "/<%= ns_table_name %>").to route_to("<%= ns_table_name %>#index")
     end
 
 <% end -%>
     it "routes to #new" do
-      get("/<%= ns_table_name %>/new").should route_to("<%= ns_table_name %>#new")
+      expect(:get => "/<%= ns_table_name %>/new").to route_to("<%= ns_table_name %>#new")
     end
 
     it "routes to #show" do
-      get("/<%= ns_table_name %>/1").should route_to("<%= ns_table_name %>#show", :id => "1")
+      expect(:get => "/<%= ns_table_name %>/1").to route_to("<%= ns_table_name %>#show", :id => "1")
     end
 
     it "routes to #edit" do
-      get("/<%= ns_table_name %>/1/edit").should route_to("<%= ns_table_name %>#edit", :id => "1")
+      expect(:get => "/<%= ns_table_name %>/1/edit").to route_to("<%= ns_table_name %>#edit", :id => "1")
     end
 
     it "routes to #create" do
-      post("/<%= ns_table_name %>").should route_to("<%= ns_table_name %>#create")
+      expect(:post => "/<%= ns_table_name %>").to route_to("<%= ns_table_name %>#create")
     end
 
     it "routes to #update" do
-      put("/<%= ns_table_name %>/1").should route_to("<%= ns_table_name %>#update", :id => "1")
+      expect(:put => "/<%= ns_table_name %>/1").to route_to("<%= ns_table_name %>#update", :id => "1")
     end
 
     it "routes to #destroy" do
-      delete("/<%= ns_table_name %>/1").should route_to("<%= ns_table_name %>#destroy", :id => "1")
+      expect(:delete => "/<%= ns_table_name %>/1").to route_to("<%= ns_table_name %>#destroy", :id => "1")
     end
 
   end
