36b5e6879d890b63ce4f61071c667e3bb183a021
vanstee <patrick@vanstee.me>
2013-07-29 22:25:07 -0400

    Split up require and module include

--- lib/rspec/rails/adapters/assertions.rb
+++ lib/rspec/rails/adapters/assertions.rb
@@ -1,11 +1,16 @@
+if ::Rails::VERSION::STRING >= '4.1.0'
+  gem 'minitest'
+  require 'minitest/assertions'
+else
+  require 'test/unit/assertions'
+end
+
+
 module RSpec::Rails::Adapters
   module Assertions
     if ::Rails::VERSION::STRING >= '4.1.0'
-      gem 'minitest'
-      require 'minitest/assertions'
       include Minitest::Assertions
     else
-      require 'test/unit/assertions'
       include Test::Unit::Assertions
     end
   end
