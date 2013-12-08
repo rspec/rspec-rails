7e8fea2c4ee1c3b38f651663f37b835bf246cd29
Andy Lindeman <andy@andylindeman.com>
2013-12-08 15:34:53 -0500

    s/match/match_array/
    Didn't come across correctly in 498907f3

--- spec/rspec/rails/matchers/relation_match_array_spec.rb
+++ spec/rspec/rails/matchers/relation_match_array_spec.rb
@@ -1,18 +1,18 @@
 require "spec_helper"
 
-describe "ActiveSupport::Relation =~ matcher" do
+describe "ActiveSupport::Relation match_array matcher" do
   before { MockableModel.delete_all }
 
   let!(:models) { Array.new(3) { MockableModel.create } }
 
   if ::Rails::VERSION::STRING >= '4'
     it "verifies that the scope returns the records on the right hand side, regardless of order" do
-      expect(MockableModel.all).to match(models.reverse)
+      expect(MockableModel.all).to match_array(models.reverse)
     end
 
     it "fails if the scope encompasses more records than on the right hand side" do
       MockableModel.create
-      expect(MockableModel.all).not_to match(models.reverse)
+      expect(MockableModel.all).not_to match_array(models.reverse)
     end
   else
     it "verifies that the scope returns the records on the right hand side, regardless of order" do
