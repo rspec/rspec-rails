36d50abd8c0cbdf58f863b57d641d9461063fe91
Jon Rowe <hello@jonrowe.co.uk>
2013-12-12 22:44:32 +1100

    Merge pull request #833 from rspec/verify_views_are_accessible_to_hooks
    Cannot stub view methods in before blocks (undefined method `view_context' for nil:NilClass)
    Conflicts:
    	lib/rspec/rails/adapters.rb

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -16,5 +16,19 @@ module Helpers
     m
   end
 
+  def with_isolated_config
+    original_config = RSpec.configuration
+    RSpec.configuration = RSpec::Core::Configuration.new
+    RSpec.configure do |c|
+      c.include RSpec::Rails::FixtureSupport
+      c.add_setting :use_transactional_fixtures, :alias_with => :use_transactional_examples
+      c.add_setting :use_instantiated_fixtures
+      c.add_setting :global_fixtures
+      c.add_setting :fixture_path
+    end
+    yield
+    RSpec.configuration = original_config
+  end
+
   RSpec.configure {|c| c.include self}
 end
