rspec/rspec-rails

commit 86aea05ee0235153068e215ff3c71a0053a8444b

use closure scope locals rather than ivars (eliminates warnings, Im not sure why the ivars are bing lost)

lib/rspec/rails/example/controller_example_group.rb
@@ -64,15 +64,16 @@ module RSpec::Rails
         end
         metadata[:example_group][:described_class].class_eval(&body)
 
+        orig_routes = nil
         before do
-          @orig_routes = self.routes
+          orig_routes = self.routes
           self.routes  = ActionDispatch::Routing::RouteSet.new.tap { |r|
             r.draw { resources :anonymous }
           }
         end
 
         after do
-          self.routes  = @orig_routes
+          self.routes  = orig_routes
           @orig_routes = nil
         end
       end

spec/spec_helper.rb
@@ -18,15 +18,17 @@ class RSpec::Core::ExampleGroup
 end
 
 RSpec.configure do |config|
+  real_world = nil
+
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.filter_run :focus
   config.run_all_when_everything_filtered = true
   config.before(:each) do
-    @real_world = RSpec.world
+    real_world = RSpec.world
     RSpec.instance_variable_set(:@world, RSpec::Core::World.new)
   end
   config.after(:each) do
-    RSpec.instance_variable_set(:@world, @real_world)
+    RSpec.instance_variable_set(:@world, real_world)
   end
   config.order = :random
 end

commit 18a2b35aec9628aa978e0ffce8be49e3b2cb37de

Fix bug introduced by 86aea05 where routes helpers cannot be used when an anonymous controller has been defined.

lib/rspec/rails/example/controller_example_group.rb
@@ -64,16 +64,15 @@ module RSpec::Rails
         end
         metadata[:example_group][:described_class].class_eval(&body)
 
-        orig_routes = nil
         before do
-          orig_routes = self.routes
+          @orig_routes = self.routes
           self.routes  = ActionDispatch::Routing::RouteSet.new.tap { |r|
             r.draw { resources :anonymous }
           }
         end
 
         after do
-          self.routes  = orig_routes
+          self.routes  = @orig_routes
           @orig_routes = nil
         end
       end