3189293fdbb502dc1a85358bacd3d8eca3635e20
Andy Lindeman <andy@andylindeman.com>
2013-12-29 16:07:47 -0500

    Explicitly including Capybara::DSL will squash the deprecation warning

--- lib/rspec/rails/vendor/capybara.rb
+++ lib/rspec/rails/vendor/capybara.rb
@@ -9,15 +9,34 @@ rescue LoadError
 end
 
 if defined?(Capybara)
-  module RSpec::Rails::CapybaraDSLDeprecated
+  module RSpec::Rails::DeprecatedCapybaraDSL
+    include ::Capybara::DSL
+
+    def self.included(mod)
+      mod.extend(ClassMethods)
+      super
+    end
+
+    module ClassMethods
+      def include(mod)
+        if mod == ::Capybara::DSL
+          self.class_variable_set(:@@_rspec_capybara_included_explicitly, true)
+        end
+
+        super
+      end
+    end
+
     ::Capybara::DSL.instance_methods(false).each do |method|
       # capybara internally calls `page`, skip to avoid a duplicate
       # deprecation warning
       next if method.to_s == 'page'
 
       define_method method do |*args, &blk|
-        RSpec.deprecate "Using the capybara method `#{method}` in controller specs",
-          :replacement => "feature specs (spec/features)"
+        unless self.class.class_variable_defined?(:@@_rspec_capybara_included_explicitly)
+          RSpec.deprecate "Using the capybara method `#{method}` in controller specs",
+            :replacement => "feature specs (spec/features)"
+        end
         super(*args, &blk)
       end
     end
@@ -25,9 +44,7 @@ if defined?(Capybara)
 
   RSpec.configure do |c|
     if defined?(Capybara::DSL)
-      c.include Capybara::DSL, :type => :controller
-      c.include ::RSpec::Rails::CapybaraDSLDeprecated, :type => :controller
-
+      c.include ::RSpec::Rails::DeprecatedCapybaraDSL, :type => :controller
       c.include Capybara::DSL, :type => :feature
     end
 
