6044ecd6f5cb571b2e4fb4a113172d34951402c9
Thomas Holmes <thomas@devminded.com>
2013-12-29 21:38:51 -0500

    Update generators and tests to use doubles/actual models

--- lib/generators/rspec/scaffold/templates/new_spec.rb
+++ lib/generators/rspec/scaffold/templates/new_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 <% output_attributes = attributes.reject{|attribute| [:datetime, :timestamp, :time, :date].index(attribute.type) } -%>
 describe "<%= ns_table_name %>/new" do
   before(:each) do
-    assign(:<%= ns_file_name %>, stub_model(<%= class_name %><%= output_attributes.empty? ? ').as_new_record)' : ',' %>
+    assign(:<%= ns_file_name %>, <%= class_name %>.new(<%= '))' if output_attributes.empty? %>
 <% output_attributes.each_with_index do |attribute, attribute_index| -%>
       :<%= attribute.name %> => <%= attribute.default.inspect %><%= attribute_index == output_attributes.length - 1 ? '' : ','%>
 <% end -%>
-<%= !output_attributes.empty? ? "    ).as_new_record)\n  end" : "  end" %>
+<%= !output_attributes.empty? ? "    ))\n  end" : "  end" %>
 
   it "renders new <%= ns_file_name %> form" do
     render
