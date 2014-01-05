c73d95dc667ec9ed5fca5e80e6cda2886099a98a
Thomas Holmes <thomas@holmes.io>
2014-01-05 12:39:48 -0500

    Deprecate `mock_model` and `stub_model`
    For rspec/rspec-rails#894

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -30,5 +30,11 @@ module Helpers
     RSpec.configuration = original_config
   end
 
+  def expect_deprecation_with_call_site(file, line)
+    expect(RSpec.configuration.reporter).to receive(:deprecation) do |options|
+      expect(options[:call_site]).to include([file, line].join(':'))
+    end
+  end
+
   RSpec.configure {|c| c.include self}
 end
