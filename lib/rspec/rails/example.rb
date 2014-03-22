1700486194023d754a3bff9133c7d814601a4f89
Xavier Shay <xavier@rhnh.net>
2014-03-22 16:37:17 -0700

    Extra inferring file type into a method.

--- lib/rspec/rails/example.rb
+++ lib/rspec/rails/example.rb
@@ -8,66 +8,6 @@ require 'rspec/rails/example/routing_example_group'
 require 'rspec/rails/example/model_example_group'
 require 'rspec/rails/example/feature_example_group'
 
-RSpec::configure do |c|
-  def c.escaped_path(*parts)
-    Regexp.compile(parts.join('[\\\/]') + '[\\\/]')
-  end
+require 'rspec/rails/configuration'
 
-  controller_path_regex = c.escaped_path(%w[spec controllers])
-  c.include RSpec::Rails::ControllerExampleGroup,
-    :type          => :controller,
-    :file_path     => lambda { |file_path, metadata|
-      metadata[:type].nil? && controller_path_regex =~ file_path
-    }
-
-  helper_path_regex = c.escaped_path(%w[spec helpers])
-  c.include RSpec::Rails::HelperExampleGroup,
-    :type          => :helper,
-    :file_path     => lambda { |file_path, metadata|
-      metadata[:type].nil? && helper_path_regex =~ file_path
-    }
-
-  mailer_path_regex = c.escaped_path(%w[spec mailers])
-  if defined?(RSpec::Rails::MailerExampleGroup)
-    c.include RSpec::Rails::MailerExampleGroup,
-      :type          => :mailer,
-      :file_path     => lambda { |file_path, metadata|
-        metadata[:type].nil? && mailer_path_regex =~ file_path
-      }
-  end
-
-  model_path_regex = c.escaped_path(%w[spec models])
-  c.include RSpec::Rails::ModelExampleGroup,
-    :type          => :model,
-    :file_path     => lambda { |file_path, metadata|
-      metadata[:type].nil? && model_path_regex =~ file_path
-    }
-
-  request_path_regex = c.escaped_path(%w[spec (requests|integration|api)])
-  c.include RSpec::Rails::RequestExampleGroup,
-    :type          => :request,
-    :file_path     => lambda { |file_path, metadata|
-      metadata[:type].nil? && request_path_regex =~ file_path
-    }
-
-  routing_path_regex = c.escaped_path(%w[spec routing])
-  c.include RSpec::Rails::RoutingExampleGroup,
-    :type          => :routing,
-    :file_path     => lambda { |file_path, metadata|
-      metadata[:type].nil? && routing_path_regex =~ file_path
-    }
-
-  view_path_regex = c.escaped_path(%w[spec views])
-  c.include RSpec::Rails::ViewExampleGroup,
-    :type          => :view,
-    :file_path     => lambda { |file_path, metadata|
-      metadata[:type].nil? && view_path_regex =~ file_path
-    }
-
-  feature_example_regex = c.escaped_path(%w[spec features])
-  c.include RSpec::Rails::FeatureExampleGroup,
-    :type          => :feature,
-    :file_path     => lambda { |file_path, metadata|
-      metadata[:type].nil? && feature_example_regex =~ file_path
-    }
-end
+RSpec::Rails.infer_spec_type_from_file_location!
