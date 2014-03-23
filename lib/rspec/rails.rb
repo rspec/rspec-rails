f4b6ffdde052e336ec945fd94fdb58e1f9341dd3
Xavier Shay <xavier@rhnh.net>
2014-03-22 17:00:28 -0700

    Enable rspec/rails to be loaded via rspec/rails/without_filetype_infer to allow opt-out of that behaviour.

--- lib/rspec/rails.rb
+++ lib/rspec/rails.rb
@@ -1,18 +1,2 @@
-require 'rspec/core'
-require 'rspec/collection_matchers'
-
-RSpec::configure do |c|
-  c.backtrace_exclusion_patterns << /vendor\//
-  c.backtrace_exclusion_patterns << /lib\/rspec\/rails/
-end
-
-require 'rails/version'
-require 'rspec/rails/extensions'
-require 'rspec/rails/view_rendering'
-require 'rspec/rails/adapters'
-require 'rspec/rails/matchers'
-require 'rspec/rails/fixture_support'
-require 'rspec/rails/example'
-require 'rspec/rails/vendor/capybara'
-
+require 'rspec/rails/without_filetype_infer'
 require 'rspec/rails/configuration'
