fbd8019edd591a35daef4f30c243f6c079226af9
Xavier Shay <xavier@rhnh.net>
2014-05-20 21:02:37 -0700

    Documentation for configuration.

--- lib/rspec/rails/configuration.rb
+++ lib/rspec/rails/configuration.rb
@@ -1,5 +1,41 @@
 module RSpec
+
   module Rails
+    # Fake class to document RSpec Rails configuration options. In practice,
+    # these are dynamically added to the normal RSpec configuration object.
+    class Configuration
+      # @!method infer_spec_type_from_file_location!
+      # Automatically tag specs in conventional directories with matching `type`
+      # metadata so that they have relevant helpers available to them. See
+      # `RSpec::Rails::DIRECTORY_MAPPINGS` for details on which metadata is
+      # applied to each directory.
+
+      # @!method render_views=(val)
+      #
+      # When set to true, controller specs will render the relevant view as
+      # well. Defaults to false.
+
+      # @!method render_views(val)
+      # Enables view rendering for controllers specs.
+
+      # @!method render_views?
+      # Reader for currently value of `render_views` setting.
+    end
+
+    # Mappings used by `infer_spec_type_from_file_location!`.
+    #
+    # @api private
+    DIRECTORY_MAPPINGS = {
+      :controller => %w[spec controllers],
+      :helper     => %w[spec helpers],
+      :mailer     => %w[spec mailers],
+      :model      => %w[spec models],
+      :request    => %w[spec (requests|integration|api)],
+      :routing    => %w[spec routing],
+      :view       => %w[spec views],
+      :feature    => %w[spec features]
+    }
+
     # @private
     def self.initialize_configuration(config)
       config.backtrace_exclusion_patterns << /vendor\//
@@ -27,7 +63,6 @@ module RSpec
       config.add_setting :global_fixtures
       config.add_setting :fixture_path
 
-      # view rendering settings
       # This allows us to expose `render_views` as a config option even though it
       # breaks the convention of other options by using `render_views` as a
       # command (i.e. render_views = true), where it would normally be used as a
@@ -36,37 +71,20 @@ module RSpec
       # but requires this workaround:
       config.add_setting :rendering_views, :default => false
 
-      # @private
-      # TODO: How to YARD this? Not actually private.
       def config.render_views=(val)
         self.rendering_views = val
       end
 
-      # @private
-      # TODO: How to YARD this? Not actually private.
       def config.render_views
         self.rendering_views = true
       end
 
-      # @private
-      # TODO: How to YARD this? Not actually private.
       def config.render_views?
         rendering_views
       end
 
-      # @private
-      # TODO: How to YARD this? Not actually private.
       def config.infer_spec_type_from_file_location!
-        {
-          :controller => %w[spec controllers],
-          :helper     => %w[spec helpers],
-          :mailer     => %w[spec mailers],
-          :model      => %w[spec models],
-          :request    => %w[spec (requests|integration|api)],
-          :routing    => %w[spec routing],
-          :view       => %w[spec views],
-          :feature    => %w[spec features]
-        }.each do |type, dir_parts|
+        DIRECTORY_MAPPINGS.each do |type, dir_parts|
           escaped_path = Regexp.compile(dir_parts.join('[\\\/]') + '[\\\/]')
           define_derived_metadata(:file_path => escaped_path) do |metadata|
             metadata[:type] ||= type
