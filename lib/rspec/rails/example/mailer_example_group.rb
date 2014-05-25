f85be5650c277ed599d5745aa7e9f5c428c7b53c
Xavier Shay <xavier@squareup.com>
2014-05-25 13:12:56 -0700

    Unbreak MailerExampleGroup loading.

--- lib/rspec/rails/example/mailer_example_group.rb
+++ lib/rspec/rails/example/mailer_example_group.rb
@@ -1,21 +1,32 @@
 module RSpec::Rails
-  # Container module for mailer spec functionality.
+  # Container module for mailer spec functionality. It is only available if
+  # ActionMailer has been loaded before it.
   module MailerExampleGroup
-    extend ActiveSupport::Concern
-    include RSpec::Rails::RailsExampleGroup
-    include ActionMailer::TestCase::Behavior
+    # This blank module is only necessary for YARD processing. It doesn't
+    # handle the conditional `defined?` check below very well.
+  end
+end
 
-    included do
-      include ::Rails.application.routes.url_helpers
-      options = ::Rails.configuration.action_mailer.default_url_options
-      options.each { |key, value| default_url_options[key] = value } if options
-    end
+if defined?(ActionMailer)
+  module RSpec::Rails
+    # Container module for mailer spec functionality.
+    module MailerExampleGroup
+      extend ActiveSupport::Concern
+      include RSpec::Rails::RailsExampleGroup
+      include ActionMailer::TestCase::Behavior
 
-    # Class-level DSL for mailer specs.
-    module ClassMethods
-      # Alias for `described_class`.
-      def mailer_class
-        described_class
+      included do
+        include ::Rails.application.routes.url_helpers
+        options = ::Rails.configuration.action_mailer.default_url_options
+        options.each { |key, value| default_url_options[key] = value } if options
+      end
+
+      # Class-level DSL for mailer specs.
+      module ClassMethods
+        # Alias for `described_class`.
+        def mailer_class
+          described_class
+        end
       end
     end
   end
