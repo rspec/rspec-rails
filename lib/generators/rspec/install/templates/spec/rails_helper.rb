b0df5fc256668bada2165562d8c0130b057a4a03
Aaron Kromer <aaron.kromer@gmail.com>
2014-05-30 00:31:34 -0400

    Remove duplicate settings between the helpers.
    The `rails_helper` only needs to be concerned with rspec-rails specific
    configurations. Any general configuration is handled by `spec_helper`.

--- lib/generators/rspec/install/templates/spec/rails_helper.rb
+++ lib/generators/rspec/install/templates/spec/rails_helper.rb
@@ -25,14 +25,6 @@ ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 <% end -%>
 RSpec.configure do |config|
-  # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
@@ -41,24 +33,18 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
-  config.order = "random"
-
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
   #
   # You can disable this behaviour by removing the line below, and instead
-  # explictly tag your specs with their type, e.g.:
+  # explicitly tag your specs with their type, e.g.:
   #
-  #     describe UsersController, :type => :controller do
+  #     RSpec.describe UsersController, :type => :controller do
   #       # ...
   #     end
   #
   # The different available types are documented in the features, such as in
-  # https://relishapp.com/rspec/rspec-rails/v/3-0/docs
+  # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 end
