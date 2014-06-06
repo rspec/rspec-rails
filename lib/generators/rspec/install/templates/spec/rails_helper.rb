b432e21b07e2c69813c8c633cbb9e9b6dbf25f6f
Juan González <opsi@opsiland.info>
2014-06-06 16:39:14 +0200

    Remove duplicated word in rails_helper's comment

--- lib/generators/rspec/install/templates/spec/rails_helper.rb
+++ lib/generators/rspec/install/templates/spec/rails_helper.rb
@@ -9,7 +9,7 @@ require 'rspec/rails'
 # run as spec files by default. This means that files in spec/support that end
 # in _spec.rb will both be required and run as specs, causing the specs to be
 # run twice. It is recommended that you do not name files matching this glob to
-# end with _spec.rb. You can configure this pattern with with the --pattern
+# end with _spec.rb. You can configure this pattern with the --pattern
 # option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
