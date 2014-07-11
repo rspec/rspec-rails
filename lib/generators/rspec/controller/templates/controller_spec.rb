9c725e26bd0b5c8fe021b194ff799870335bb678
Thomas Kriechbaumer <kriechbaumer@gmail.com>
2014-07-11 19:27:11 +0200

    make single controller_spec template conform with scaffolded controller_spec

--- lib/generators/rspec/controller/templates/controller_spec.rb
+++ lib/generators/rspec/controller/templates/controller_spec.rb
@@ -4,9 +4,9 @@ require 'rails_helper'
 RSpec.describe <%= class_name %>Controller, :type => :controller do
 
 <% for action in actions -%>
-  describe "GET '<%= action %>'" do
+  describe "GET <%= action %>" do
     it "returns http success" do
-      get '<%= action %>'
+      get :<%= action %>
       expect(response).to have_http_status(:success)
     end
   end
