38c4e30fa53fc1b1a67ade57ae892acfe5134dfb
Josh Kalderimis <josh.kalderimis@gmail.com>
2014-08-20 12:44:02 +0200

    moar retries, more jobs

--- templates/travis_retry_bundle_install.sh
+++ templates/travis_retry_bundle_install.sh
@@ -3,4 +3,4 @@
 set -ex
 source FUNCTIONS_SCRIPT_FILE
 
-travis_retry eval "bundle install --gemfile ./Gemfile --path REPLACE_BUNDLE_PATH"
+travis_retry eval "bundle install --gemfile ./Gemfile --path REPLACE_BUNDLE_PATH --retry=3 --jobs=3"
