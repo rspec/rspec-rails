09ad7b34a10775115752c34d31f86a0dad13898e
Aaron Kromer <aaron.kromer@gmail.com>
2014-08-21 11:15:30 -0400

    Include smoke task for non-ActiveRecord apps.
    Creates a 2nd set of generator and smoke tasks which target an example
    app which does not use ActiveRecord. In order to accomplish this, a
    custom in-memory model is used which supports enough of the AR protocol
    to allow default specs to pass.
    
    Use a custom "in-memory" model as the ORM replacement. This is a simple
    stand-in allowing us to continue to use all of the other Rails
    generators. It also prevents us from needing to pull in another ORM gem
    such as such as Mongoid, DataMapper, ROM, etc.
    
    The custom "model" is necessary as the non-ActiveRecord example app's
    specs fail with `NameError`s. This is due to the other Rails' generators
    expecting to have a "model" type class available, but Rails is unable to
    load it.  Just adding the classes does not work as the generated
    controllers, form helpers, rendering process, etc. are so tightly
    coupled to the `ActiveModel` API interface that all sorts of things
    start failing.

--- templates/generate_stuff.rb
+++ templates/generate_stuff.rb
@@ -1,3 +1,61 @@
+module ExampleAppHooks
+  DEFAULT_SOURCE_PATH = File.expand_path('..', __FILE__)
+
+  module AR
+    def source_paths
+      [DEFAULT_SOURCE_PATH]
+    end
+
+    def setup_tasks
+      # no-op
+    end
+
+    def final_tasks
+      copy_file 'spec/verify_active_record_spec.rb'
+      run('bin/rake db:migrate')
+      if ::Rails::VERSION::STRING.to_f < 4.1
+        run('bin/rake db:migrate RAILS_ENV=test')
+      end
+    end
+  end
+
+  module NoAR
+    def source_paths
+      [File.join(DEFAULT_SOURCE_PATH, 'no_active_record')]
+    end
+
+    def setup_tasks
+      copy_file 'app/models/in_memory/model.rb'
+      copy_file 'lib/rails/generators/in_memory/model/model_generator.rb'
+      copy_file 'lib/rails/generators/in_memory/model/templates/model.rb.erb'
+      application <<-CONFIG
+        config.generators do |g|
+          g.orm :in_memory, :migration => false
+        end
+      CONFIG
+    end
+
+    def final_tasks
+      copy_file 'spec/verify_no_active_record_spec.rb'
+    end
+  end
+
+  def self.environment_hooks
+    if defined?(ActiveRecord)
+      AR
+    else
+      NoAR
+    end
+  end
+end
+
+# Generally polluting `main` is bad as it monkey patches all objects. In this
+# context, `self` is an _instance_ of a `Rails::Generators::AppGenerator`. So
+# this won't pollute anything.
+extend ExampleAppHooks.environment_hooks
+
+setup_tasks
+
 generate('rspec:install')
 generate('controller wombats index') # plural
 generate('controller welcome index') # singular
@@ -12,13 +70,16 @@ generate('scaffold admin/account name:string') # scaffold with nested resource
 
 generate('controller things custom_action')
 
-file "app/views/things/custom_action.html.erb", "This is a template for a custom action.", {:force=>true}
+file "app/views/things/custom_action.html.erb",
+     "This is a template for a custom action.",
+     :force => true
 
 gsub_file 'spec/spec_helper.rb', /^=(begin|end)/, ''
 
-if defined?(ActiveRecord)
-  run('bin/rake db:migrate')
-  if ::Rails::VERSION::STRING.to_f < 4.1
-    run('bin/rake db:migrate RAILS_ENV=test')
-  end
-end
+# Warnings are too noisy in the sample apps
+gsub_file 'spec/spec_helper.rb',
+          'config.warnings = true',
+          'config.warnings = false'
+gsub_file '.rspec', '--warnings', ''
+
+final_tasks
