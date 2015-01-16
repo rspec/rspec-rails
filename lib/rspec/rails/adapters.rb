e53c23ed33b488f95fe9728950ed010bfc64faab
Aaron Kromer <aaron.kromer@gmail.com>
2015-01-16 10:19:19 -0500

    Turn off Test::Unit's autorun.
    Test::Unit "helpfully" sets up autoload for its `AutoRunner`.  While we
    do not use reference it directly, when we load the `TestCase` classes
    from ActiveSupport, AS kindly references `AutoRunner` for everyone.
    
    We need to pre-emptively load 'test/unit' and make sure the version
    installed has `AutoRunner` (the 3.x line does so far). If so, we turn it
    off.

--- lib/rspec/rails/adapters.rb
+++ lib/rspec/rails/adapters.rb
@@ -27,7 +27,17 @@ module RSpec
       elsif ::Rails::VERSION::STRING >= '3.2.21'
         # TODO: Change the above check to >= '3.2.22' once it's released
         begin
+          # Test::Unit "helpfully" sets up autoload for its `AutoRunner`.
+          # While we do not use reference it directly, when we load the
+          # `TestCase` classes from ActiveSupport, AS kindly references
+          # `AutoRunner` for everyone.
+          #
+          # We need to pre-emptively load 'test/unit' and make sure the version
+          # installed has `AutoRunner` (the 3.x line does so far). If so, we
+          # turn it off.
+          require 'test/unit'
           require 'test/unit/assertions'
+          Test::Unit::AutoRunner.need_auto_run = false if defined?(Test::Unit::AutoRunner)
         rescue LoadError => e
           raise LoadError, <<-ERR.squish, e.backtrace
             Ruby 2.2+ has removed test/unit from the core library. Rails
