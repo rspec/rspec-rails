f9676ae4e8e9b5656a876105dcff27aba5a46aca
Aaron Kromer <aaron.kromer@gmail.com>
2015-02-02 13:22:21 -0500

    Remove unnecessary code :skull:
    The matchers no longer attempt to use `AssertionFailedError` instead
    they use `ActiveSupport::TestCase::Assertion`.
    
    Fix #1294

--- lib/rspec/rails/matchers.rb
+++ lib/rspec/rails/matchers.rb
@@ -9,17 +9,6 @@ module RSpec
   end
 end
 
-begin
-  require 'test/unit/assertionfailederror'
-rescue LoadError
-  module Test
-    module Unit
-      class AssertionFailedError < StandardError
-      end
-    end
-  end
-end
-
 require 'rspec/rails/matchers/have_rendered'
 require 'rspec/rails/matchers/redirect_to'
 require 'rspec/rails/matchers/routing_matchers'
