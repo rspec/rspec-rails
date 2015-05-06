af53b75c201ebc49b139e62c2d78825beb095838
Gabe Martin-Dempesy <gabe@zendesk.com>
2015-05-05 20:27:26 -0700

    only mix RailsExampleGroup into JobExampleGroup if ActiveJob is defined

--- lib/rspec/rails/example/job_example_group.rb
+++ lib/rspec/rails/example/job_example_group.rb
@@ -1,10 +1,20 @@
 module RSpec
   module Rails
-    # Container class for job spec functionality. Does not provide anything
-    # special over the common RailsExampleGroup currently.
+    # Container module for job spec functionality. It is only available if
+    # ActiveJob has been loaded before it.
     module JobExampleGroup
-      extend ActiveSupport::Concern
-      include RSpec::Rails::RailsExampleGroup
+    end
+  end
+end
+
+if defined?(ActiveJob)
+  module RSpec
+    module Rails
+      # Container module for job spec functionality.
+      module JobExampleGroup
+        extend ActiveSupport::Concern
+        include RSpec::Rails::RailsExampleGroup
+      end
     end
   end
 end
--- spec/rspec/rails/example/job_example_group_spec.rb
+++ spec/rspec/rails/example/job_example_group_spec.rb
@@ -2,7 +2,9 @@ require "spec_helper"
 
 module RSpec::Rails
   describe JobExampleGroup do
-    it_behaves_like "an rspec-rails example group mixin", :job,
-      './spec/jobs/', '.\\spec\\jobs\\'
+    if defined?(ActiveJob)
+      it_behaves_like "an rspec-rails example group mixin", :job,
+        './spec/jobs/', '.\\spec\\jobs\\'
+    end
   end
 end
