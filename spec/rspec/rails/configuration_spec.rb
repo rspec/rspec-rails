b78356f36b18aa672fc3aec9edc154126335f628
Aaron Kromer <aaron.kromer@gmail.com>
2015-05-13 19:33:57 -0400

    Update spec to remove hooks.
    This changes to nearly mirrors how the configuration is tested in rspec-core.
    Instead of setting up a new configuration object each test run, we clone
    the existing object. This does two things:
    
    - prevents polluting the actual test configuration
    - instead of creating a new configuration it clones the existing one to
      ensure we actually modify the RSpec config

--- spec/rspec/rails/configuration_spec.rb
+++ spec/rspec/rails/configuration_spec.rb
@@ -1,26 +1,20 @@
 require "spec_helper"
 
-describe "configuration" do
-  before do
-    @orig_render_views = RSpec.configuration.render_views?
-  end
+RSpec.describe "Configuration" do
 
-  after do
-    RSpec.configuration.render_views = @orig_render_views
-  end
+  subject(:config) { RSpec.configuration.clone }
 
   describe "#render_views?" do
     it "is false by default" do
-      expect(RSpec.configuration.render_views?).to be_falsey
+      expect(config.render_views?).to be_falsey
     end
   end
 
   describe "#render_views" do
     it "sets render_views? to return true" do
-      RSpec.configuration.render_views = false
-      RSpec.configuration.render_views
-
-      expect(RSpec.configuration.render_views?).to be_truthy
+      expect {
+        config.render_views
+      }.to change { config.render_views? }.to be_truthy
     end
   end
 end
