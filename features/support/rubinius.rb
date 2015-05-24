8197094cfad8a9423cf0429b34fe630db20c8e4b
Joe Grossberg <josephgrossberg+github@gmail.com>
2015-05-24 12:06:57 -0700

    consistent quoting, more readable interpolation
    via http://refactorcop.com/rspec/rspec-rails

--- features/support/rubinius.rb
+++ features/support/rubinius.rb
@@ -1,5 +1,5 @@
 # Required until https://github.com/rubinius/rubinius/issues/2430 is resolved
-ENV['RBXOPT'] = "#{ENV["RBXOPT"]} -Xcompiler.no_rbc"
+ENV['RBXOPT'] = "#{ENV['RBXOPT']} -Xcompiler.no_rbc"
 
 Around "@unsupported-on-rbx" do |scenario, block|
   block.call unless defined?(Rubinius)
