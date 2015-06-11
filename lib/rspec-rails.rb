92e57ee467b76ed1e67945af3e8da4998611e6db
Aaron Kromer <aaron.kromer@gmail.com>
2015-06-11 13:42:00 -0400

    Re-write the Railtie to use a modular features
    This is the first step at trying to improve the ability to support
    multiple Rails versions. It extracts the majority of the logic for
    `ActionMailer` configuration into clear modules.
    
    In addition to making the code cleaner, this allows the ancestor chain
    to be inspected to understand what configurations settings are included.

--- lib/rspec-rails.rb
+++ lib/rspec-rails.rb
@@ -1,18 +1,87 @@
 require 'rspec/rails/feature_check'
 
+module RSpec
+  module Rails
+    # private
+    module Adapters
+      # private
+      module NoActionMailer
+        # private
+        def setup_preview_path(config)
+        end
+      end
+
+      # private
+      module ActionMailerPreviewPath
+        # private
+        def setup_preview_path(config)
+          return unless show_previews?(config.action_mailer)
+
+          # Logic taken from the Action Mailer Railtie
+          config.action_mailer.preview_path ||= default_preview_path
+        end
+
+        # private
+        def show_previews?(options)
+          ::Rails.env.development?
+        end
+
+        # private
+        def default_preview_path
+          # Logic taken from the Action Mailer Railtie
+          defined?(::Rails.root) ? "#{::Rails.root}/spec/mailers/previews" : nil
+        end
+      end
+
+      # private
+      module ActionMailerShowPreviews
+        include ActionMailerPreviewPath
+
+        # private
+        def show_previews?(options)
+          # Logic taken from the Action Mailer Railtie
+          options.show_previews = super if options.show_previews.nil?
+          options.show_previews
+        end
+      end
+
+      # private
+      #
+      # Checking for `ActionMailer` will not cause the configuration to load.
+      # That happens on loading `ActionMailer::Base`, so this check should be
+      # safe. The eager loading of the config on `ActionMailer::Base` also
+      # prevents us from checking if the `preview_path` and `show_preview`
+      # options are defined; so we switch on the Rails version.
+      ActionMailerRailtie = if !defined?(::ActionMailer)
+                              NoActionMailer
+                            elsif ::Rails::VERSION::STRING > '4.2'
+                              ActionMailerShowPreviews
+                            elsif ::Rails::VERSION::STRING > '4.1'
+                              ActionMailerPreviewPath
+                            else
+                              NoActionMailer
+                            end
+    end
+  end
+end
+
 # Namespace for all core RSpec projects.
 module RSpec
   # Namespace for rspec-rails code.
   module Rails
     # Railtie to hook into Rails.
     class Railtie < ::Rails::Railtie
+      include RSpec::Rails::Adapters::ActionMailerRailtie
+
       # Rails-3.0.1 requires config.app_generators instead of 3.0.0's config.generators
       generators = config.respond_to?(:app_generators) ? config.app_generators : config.generators
       generators.integration_tool :rspec
       generators.test_framework :rspec
 
       generators do
-        ::Rails::Generators.hidden_namespaces.reject! { |namespace| namespace.start_with?("rspec") }
+        ::Rails::Generators.hidden_namespaces.reject! do |namespace|
+          namespace.start_with?("rspec")
+        end
       end
 
       rake_tasks do
@@ -22,48 +91,8 @@ module RSpec
       # This is called after the environment has been loaded but before Rails
       # sets the default for the `preview_path`
       initializer "rspec_rails.action_mailer",
-                  :before => "action_mailer.set_configs" do |app|
-        setup_preview_path(app)
-      end
-
-    private
-
-      def setup_preview_path(app)
-        return unless supports_action_mailer_previews?(app.config)
-        options = app.config.action_mailer
-        config_default_preview_path(options) if config_preview_path?(options)
-      end
-
-      def config_preview_path?(options)
-        # We cannot use `respond_to?(:show_previews)` here as it will always
-        # return `true`.
-        if ::Rails::VERSION::STRING < '4.2'
-          ::Rails.env.development?
-        elsif options.show_previews.nil?
-          options.show_previews = ::Rails.env.development?
-        else
-          options.show_previews
-        end
-      end
-
-      def config_default_preview_path(options)
-        return unless options.preview_path.blank?
-        options.preview_path = "#{::Rails.root}/spec/mailers/previews"
-      end
-
-      def supports_action_mailer_previews?(config)
-        # These checks avoid loading `ActionMailer`. Using `defined?` has the
-        # side-effect of the class getting loaded if it is available. This is
-        # problematic because loading `ActionMailer::Base` will cause it to
-        # read the config settings; this is the only time the config is read.
-        # If the config is loaded now, any settings declared in a config block
-        # in an initializer will be ignored.
-        #
-        # If the action mailer railtie has not been loaded then `config` will
-        # not respond to the method. However, we cannot use
-        # `config.action_mailer.respond_to?(:preview_path)` here as it will
-        # always return `true`.
-        config.respond_to?(:action_mailer) && ::Rails::VERSION::STRING > '4.1'
+        :before => "action_mailer.set_configs" do |app|
+        setup_preview_path app.config
       end
     end
   end
