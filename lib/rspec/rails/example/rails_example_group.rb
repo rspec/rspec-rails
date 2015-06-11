8c4ec6b09a5461ae2503964bcc89d601832aa474
Aaron Kromer <aaron.kromer@gmail.com>
2015-06-11 14:42:09 -0400

    Use the adapters for setting up base examples
    This refactors some of the modules which are included by default in the
    base Rails example group. It does a little constant renaming to support
    backwards compatibility, while workings towards a more modular design.

--- lib/rspec/rails/example/rails_example_group.rb
+++ lib/rspec/rails/example/rails_example_group.rb
@@ -4,12 +4,32 @@ require 'rspec/rails/matchers'
 
 module RSpec
   module Rails
+    # private
+    module Adapters
+      # private
+      module NoMinitestLifecycle
+      end
+
+      # private
+      SetupAndTeardown  = RSpec::Rails::SetupAndTeardownAdapter
+
+      # private
+      MinitestAssertion = RSpec::Rails::MinitestAssertionAdapter
+
+      # private
+      MinitestLifecycle = if ::Rails::VERSION::STRING >= '4'
+                            RSpec::Rails::MinitestLifecycleAdapter
+                          else
+                            NoMinitestLifecycle
+                          end
+    end
+
     # Common rails example functionality.
     module RailsExampleGroup
       extend ActiveSupport::Concern
-      include RSpec::Rails::SetupAndTeardownAdapter
-      include RSpec::Rails::MinitestLifecycleAdapter if ::Rails::VERSION::STRING >= '4'
-      include RSpec::Rails::MinitestAssertionAdapter
+      include RSpec::Rails::Adapters::SetupAndTeardown
+      include RSpec::Rails::Adapters::MinitestLifecycle
+      include RSpec::Rails::Adapters::MinitestAssertion
       include RSpec::Rails::Matchers
       include RSpec::Rails::FixtureSupport
     end
