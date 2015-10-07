b35dc1ca2c9d31a25b66c058128434c30bbb37de
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2015-10-07 18:19:38 +0200

    Change have_enqueued_jobs usage UI

--- lib/rspec/rails/matchers/active_job.rb
+++ lib/rspec/rails/matchers/active_job.rb
@@ -9,9 +9,9 @@ module RSpec
       module ActiveJob
         # @private
         class HaveEnqueuedJobs < RSpec::Matchers::BuiltIn::BaseMatcher
-          def initialize(number, only = nil)
-            @number = number
-            @only   = only
+          def initialize(only)
+            @only = only
+            set_expected_number(:exactly, 1)
           end
 
           def matches?(proc)
@@ -20,15 +20,47 @@ module RSpec
             before_block_jobs_size = enqueued_jobs_size(@only)
             proc.call
             @in_block_jobs_size = enqueued_jobs_size(@only) - before_block_jobs_size
-            @number == @in_block_jobs_size
+
+            case @expectation_type
+            when :exactly then @expected_number == @in_block_jobs_size
+            when :at_most then @expected_number >= @in_block_jobs_size
+            when :at_least then @expected_number <= @in_block_jobs_size
+            end
+          end
+
+          def exactly(count)
+            set_expected_number(:exactly, count)
+            self
+          end
+
+          def at_least(count)
+            set_expected_number(:at_least, count)
+            self
+          end
+
+          def at_most(count)
+            set_expected_number(:at_most, count)
+            self
+          end
+
+          def times
+            self
           end
 
           def failure_message
-            "expected to enqueue #{@number} jobs, but enqueued #{@in_block_jobs_size}"
+            "expected to enqueue #{message_expectation_modifier} #{@expected_number} jobs, but enqueued #{@in_block_jobs_size}"
           end
 
           def failure_message_when_negated
-            "expected not to enqueue #{@number} jobs, but enqueued #{@in_block_jobs_size}"
+            "expected not to enqueue #{message_expectation_modifier} #{@expected_number} jobs, but enqueued #{@in_block_jobs_size}"
+          end
+
+          def message_expectation_modifier
+            case @expectation_type
+            when :exactly then "exactly"
+            when :at_most then "at most"
+            when :at_least then "at least"
+            end
           end
 
           def supports_block_expectations?
@@ -38,11 +70,21 @@ module RSpec
         private
 
           def enqueued_jobs_size(only)
-            if only
-              queue_adapter.enqueued_jobs.count { |job| Array(only).include?(job.fetch(:job)) }
+            if only.any?
+              queue_adapter.enqueued_jobs.count { |job| only.include?(job.fetch(:job)) }
             else
               queue_adapter.enqueued_jobs.count
             end
+          end
+
+          def set_expected_number(relativity, count)
+            @expectation_type = relativity
+            @expected_number = case count
+                               when Numeric then count
+                               when :once then 1
+                               when :twice then 2
+                               when :thrice then 3
+                               end
           end
 
           def queue_adapter
@@ -52,20 +94,29 @@ module RSpec
       end
 
       # @api public
-      # Passess if `number` of jobs were enqueued inside block
+      # Passess if `count` of jobs were enqueued inside block
       #
       # @example
       #     expect {
       #       HeavyLiftingJob.perform_later
-      #     }.to have_enqueued_jobs(1)
+      #     }.to have_enqueued_jobs
       #
       #     expect {
       #       HelloJob.perform_later
       #       HeavyLiftingJob.perform_later
-      #     }.to have_enqueued_jobs(1, only: HelloJob)
-      def have_enqueued_jobs(number = 1, options = {})
-        only = options[:only]
-        ActiveJob::HaveEnqueuedJobs.new(number, only)
+      #     }.to have_enqueued_jobs(HelloJob).exactly(:once)
+      #
+      #     expect {
+      #       HelloJob.perform_later
+      #       HelloJob.perform_later
+      #       HelloJob.perform_later
+      #     }.to have_enqueued_jobs(HelloJob).at_least(2).times
+      #
+      #     expect {
+      #       HelloJob.perform_later
+      #     }.to have_enqueued_jobs(HelloJob).at_most(:twice)
+      def have_enqueued_jobs(*jobs)
+        ActiveJob::HaveEnqueuedJobs.new(jobs)
       end
     end
   end
--- spec/rspec/rails/matchers/active_job_spec.rb
+++ spec/rspec/rails/matchers/active_job_spec.rb
@@ -32,21 +32,21 @@ RSpec.describe "ActiveJob matchers", :skip => !RSpec::Rails::FeatureCheck.has_ac
   describe "have_enqueued_jobs" do
     it "raises ArgumentError when no Proc passed to expect" do
       expect {
-        expect(heavy_lifting_job.perform_later).to have_enqueued_jobs(1)
+        expect(heavy_lifting_job.perform_later).to have_enqueued_jobs
       }.to raise_error(ArgumentError)
     end
 
-    it "passess with correct number" do
+    it "passess with default one number" do
       expect {
         heavy_lifting_job.perform_later
-      }.to have_enqueued_jobs(1)
+      }.to have_enqueued_jobs
     end
 
     it "counts only jobs enqueued in block" do
       heavy_lifting_job.perform_later
       expect {
         heavy_lifting_job.perform_later
-      }.to have_enqueued_jobs(1)
+      }.to have_enqueued_jobs.exactly(1)
     end
 
     it "passess when negated" do
@@ -55,8 +55,8 @@ RSpec.describe "ActiveJob matchers", :skip => !RSpec::Rails::FeatureCheck.has_ac
 
     it "fails when job is not enqueued" do
       expect {
-        expect { }.to have_enqueued_jobs(1)
-      }.to raise_error(/expected to enqueue 1 jobs, but enqueued 0/)
+        expect { }.to have_enqueued_jobs
+      }.to raise_error(/expected to enqueue exactly 1 jobs, but enqueued 0/)
     end
 
     it "fails when too many jobs enqueued" do
@@ -64,36 +64,85 @@ RSpec.describe "ActiveJob matchers", :skip => !RSpec::Rails::FeatureCheck.has_ac
         expect {
           heavy_lifting_job.perform_later
           heavy_lifting_job.perform_later
-        }.to have_enqueued_jobs(1)
-      }.to raise_error(/expected to enqueue 1 jobs, but enqueued 2/)
+        }.to have_enqueued_jobs.exactly(1)
+      }.to raise_error(/expected to enqueue exactly 1 jobs, but enqueued 2/)
     end
 
     it "reports correct number in fail error message" do
       heavy_lifting_job.perform_later
       expect {
-        expect { }.to have_enqueued_jobs(1)
-      }.to raise_error(/expected to enqueue 1 jobs, but enqueued 0/)
+        expect { }.to have_enqueued_jobs.exactly(1)
+      }.to raise_error(/expected to enqueue exactly 1 jobs, but enqueued 0/)
     end
 
     it "fails when negated and job is enqueued" do
       expect {
         expect { heavy_lifting_job.perform_later }.not_to have_enqueued_jobs
-      }.to raise_error(/expected not to enqueue 1 jobs, but enqueued 1/)
+      }.to raise_error(/expected not to enqueue exactly 1 jobs, but enqueued 1/)
     end
 
-    it "passes with only option" do
+    it "passes with job name" do
       expect {
         hello_job.perform_later
         heavy_lifting_job.perform_later
-      }.to have_enqueued_jobs(1, :only => hello_job)
+      }.to have_enqueued_jobs(hello_job).exactly(1).times
     end
 
-    it "passes with only option as array" do
+    it "passes with multiple job names" do
       expect {
         hello_job.perform_later
         logging_job.perform_later
         heavy_lifting_job.perform_later
-      }.to have_enqueued_jobs(2, :only => [hello_job, logging_job])
+      }.to have_enqueued_jobs(hello_job, logging_job).exactly(2).times
+    end
+
+    it "passess with :once count" do
+      expect {
+        hello_job.perform_later
+      }.to have_enqueued_jobs.exactly(:once)
+    end
+
+    it "passess with :twice count" do
+      expect {
+        hello_job.perform_later
+        hello_job.perform_later
+      }.to have_enqueued_jobs.exactly(:twice)
+    end
+
+    it "passess with :thrice count" do
+      expect {
+        hello_job.perform_later
+        hello_job.perform_later
+        hello_job.perform_later
+      }.to have_enqueued_jobs.exactly(:thrice)
+    end
+
+    it "passess with at_least count when enqueued jobs are over limit" do
+      expect {
+        hello_job.perform_later
+        hello_job.perform_later
+      }.to have_enqueued_jobs.at_least(:once)
+    end
+
+    it "passess with at_most count when enqueued jobs are under limit" do
+      expect {
+        hello_job.perform_later
+      }.to have_enqueued_jobs.at_most(:once)
+    end
+
+    it "generates failure message with at least hint" do
+      expect {
+        expect { }.to have_enqueued_jobs.at_least(:once)
+      }.to raise_error(/expected to enqueue at least 1 jobs, but enqueued 0/)
+    end
+
+    it "generates failure message with at most hint" do
+      expect {
+        expect {
+          hello_job.perform_later
+          hello_job.perform_later
+        }.to have_enqueued_jobs.at_most(:once)
+      }.to raise_error(/expected to enqueue at most 1 jobs, but enqueued 2/)
     end
   end
 end
