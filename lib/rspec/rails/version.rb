3f3f50d0f54ec69b3ed2825766e0a6171a4c1083
Sam Phippen <samphippen@googlemail.com>
2016-04-02 11:22:38 -0400

    Revert "Temporarily set version for release of 3.5.0.beta3"
    This reverts commit d707c93d95911db76b107dd133c49661deb0403c.

--- lib/rspec/rails/version.rb
+++ lib/rspec/rails/version.rb
@@ -3,7 +3,7 @@ module RSpec
     # Version information for RSpec Rails.
     module Version
       # Current version of RSpec Rails, in semantic versioning format.
-      STRING = '3.5.0.beta3'
+      STRING = '3.5.0.pre'
     end
   end
 end
