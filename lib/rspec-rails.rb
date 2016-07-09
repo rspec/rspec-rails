73b742cc22eb5ac038fb1a8db1c4285da2daf329
John Meehan <john.meehan1@gmail.com>
2016-07-09 11:47:35 +0100

    Rake notes compatibility in Rails 5.1.0 (#1661)
    * As of Rails 5.1.0 gems can register a directories with the
    Rails `SourceAnnotationExtractor` which means when we 
    run `rake notes` it will now also list out any todos in the
    `/spec` folder.
    
    This was added in this PR:
    https://github.com/rails/rails/pull/25692
    
    Resulting in:
    
    ```bash
    
    $ rake notes
    app/spec/models/article_spec.rb:
      * [2] [TODO] add more specs
      * [3] [FIXME] Spec A is broken
      * [4] [OPTIMIZE] improve this test
    
    ```
    
    * In this commit:
    * just fixed the railtie
    
    As of Rails 5.1.0 gems can register a directories to work with `rake
    notes`
    
    In this case we can tell Rails it to now look in the `/spec` folder when
    searching for annotations like `# TODO:` `# FIXME` etc.
    
    This was added in this PR:
    rails/rails#25692
    
    Resulting in:
    
    ```bash
    
    $ rake notes
    app/spec/models/article_spec.rb:
      * [2] [TODO] add more specs
      * [3] [FIXME] Spec A is broken
      * [4] [OPTIMIZE] improve this test
    
    ```

--- lib/rspec-rails.rb
+++ lib/rspec-rails.rb
@@ -8,6 +8,10 @@ module RSpec
   module Rails
     # Railtie to hook into Rails.
     class Railtie < ::Rails::Railtie
+      # As of Rails 5.1.0 you can register directories to work with `rake notes`
+      if ::Rails::VERSION::STRING >= '5.1'
+        SourceAnnotationExtractor::Annotation.register_directories("spec")
+      end
       # Rails-3.0.1 requires config.app_generators instead of 3.0.0's config.generators
       generators = config.respond_to?(:app_generators) ? config.app_generators : config.generators
       generators.integration_tool :rspec
