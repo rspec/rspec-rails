213029c7130282a1f84ab5ddab0a157fc549687d
Kevin Glowacz <kevin@glowacz.info>
2017-03-21 21:15:17 -0500

    Load spec_helper before Rails
    so that things in spec_helper, especially coverage tools,
    have a chance to load before the Rails code loads

--- lib/generators/rspec/install/templates/spec/rails_helper.rb
+++ lib/generators/rspec/install/templates/spec/rails_helper.rb
@@ -1,9 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
