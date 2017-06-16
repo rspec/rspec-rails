3e5ad343ffed68a41d09bbd97d5492c2369d8736
Sam Phippen <samphippen@googlemail.com>
2017-06-16 17:33:42 -0400

    Fix a non-system-test-bug

--- spec/rspec/rails/example/system_example_group_spec.rb
+++ spec/rspec/rails/example/system_example_group_spec.rb
@@ -1,7 +1,7 @@
-if defined?(RSpec::Rails::SystemExampleGroup)
-  require "spec_helper"
+require "spec_helper"
 
-  module RSpec::Rails
+module RSpec::Rails
+  if defined?(SystemExampleGroup)
     describe SystemExampleGroup do
       it_behaves_like "an rspec-rails example group mixin", :system,
         './spec/system/', '.\\spec\\system\\'
