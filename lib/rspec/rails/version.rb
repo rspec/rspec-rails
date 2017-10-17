d707ae879aa7362c302b8fc2500c677283e07e9b
Sam Phippen <samphippen@googlemail.com>
2017-10-17 15:53:06 +0100

    Releases 3.7.0

--- lib/rspec/rails/version.rb
+++ lib/rspec/rails/version.rb
@@ -3,7 +3,7 @@ module RSpec
     # Version information for RSpec Rails.
     module Version
       # Current version of RSpec Rails, in semantic versioning format.
-      STRING = '3.7.0.pre'
+      STRING = '3.7.0'
     end
   end
 end
