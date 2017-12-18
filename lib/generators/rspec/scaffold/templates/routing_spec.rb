e4a20bf1f38aefef169caa406e86cadc2ae32262
Nikola Đuza <nikolaseap@gmail.com>
2017-12-18 13:25:26 +0100

    Remove starting new line after describe block

--- lib/generators/rspec/scaffold/templates/routing_spec.rb
+++ lib/generators/rspec/scaffold/templates/routing_spec.rb
@@ -3,18 +3,17 @@ require "rails_helper"
 <% module_namespacing do -%>
 RSpec.describe <%= controller_class_name %>Controller, <%= type_metatag(:routing) %> do
   describe "routing" do
-
 <% unless options[:singleton] -%>
     it "routes to #index" do
       expect(:get => "/<%= ns_table_name %>").to route_to("<%= ns_table_name %>#index")
     end
-    
+
 <% end -%>
 <% unless options[:api] -%>
     it "routes to #new" do
       expect(:get => "/<%= ns_table_name %>/new").to route_to("<%= ns_table_name %>#new")
     end
-  
+
 <% end -%>
     it "routes to #show" do
       expect(:get => "/<%= ns_table_name %>/1").to route_to("<%= ns_table_name %>#show", :id => "1")
