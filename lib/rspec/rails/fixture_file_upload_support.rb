ba7aeb392fa9a7b242d43e51cc496112d3e646b8
Laurent Cobos <laurent@11factory.fr>
2018-01-11 22:55:19 -0500

    [fix-1921] use relative path when no fixture_path

--- lib/rspec/rails/fixture_file_upload_support.rb
+++ lib/rspec/rails/fixture_file_upload_support.rb
@@ -8,7 +8,7 @@ module RSpec
 
       def rails_fixture_file_wrapper
         resolved_fixture_path = (fixture_path || RSpec.configuration.fixture_path || '')
-        RailsFixtureFileWrapper.fixture_path = File.join(resolved_fixture_path, '')
+        RailsFixtureFileWrapper.fixture_path = File.join(resolved_fixture_path, '') unless resolved_fixture_path.strip.empty?
         RailsFixtureFileWrapper.instance
       end
 
@@ -18,7 +18,7 @@ module RSpec
         class << self
           attr_reader :fixture_path
 
-          # Get the instance of wrapper
+          # Get instance of wrapper
           def instance
             @instance ||= new
           end
--- spec/rspec/rails/fixture_file_upload_support_spec.rb
+++ spec/rspec/rails/fixture_file_upload_support_spec.rb
@@ -6,24 +6,32 @@ module RSpec::Rails
       before { RSpec.configuration.fixture_path = File.dirname(__FILE__) }
 
       it 'resolves fixture file' do
-        expect(fixture_file_upload_resolved.run).to be true
+        expect(fixture_file_upload_resolved('fixture_file_upload_support_spec.rb').run).to be true
       end
     end
 
     context 'with fixture path set in spec' do
       it 'resolves fixture file' do
-        expect(fixture_file_upload_resolved(File.dirname(__FILE__)).run).to be true
+        expect(fixture_file_upload_resolved('fixture_file_upload_support_spec.rb', File.dirname(__FILE__)).run).to be true
       end
     end
 
-    def fixture_file_upload_resolved(fixture_path = nil)
+    context 'with fixture path not set' do
+      before { RSpec.configuration.fixture_path = nil }
+
+      it 'resolves fixture with relative path' do
+        expect(fixture_file_upload_resolved('fixture_file_upload_support_spec.rb').run).to be true
+      end
+    end
+
+    def fixture_file_upload_resolved(fixture_name, fixture_path = nil)
       RSpec::Core::ExampleGroup.describe do
         include RSpec::Rails::FixtureFileUploadSupport
 
         self.fixture_path = fixture_path if fixture_path
 
         it 'supports fixture file upload' do
-          file = fixture_file_upload(File.basename(__FILE__))
+          file = fixture_file_upload(fixture_name)
           expect(file.read).to match(/describe FixtureFileUploadSupport/im)
         end
       end
