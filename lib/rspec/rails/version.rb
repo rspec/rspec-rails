0addc2e83fc90759568838b3feb3b92d1caca9b2
Myron Marston <myron.marston@gmail.com>
2018-08-04 12:45:29 -0700

    Release 3.8.0.

--- lib/rspec/rails/version.rb
+++ lib/rspec/rails/version.rb
@@ -3,7 +3,7 @@ module RSpec
     # Version information for RSpec Rails.
     module Version
       # Current version of RSpec Rails, in semantic versioning format.
-      STRING = '3.8.pre'
+      STRING = '3.8.0'
     end
   end
 end
