1692d1dde3cb6e6d55c414629224b21a172162fd
Joel Lubrano <joel.lubrano@stitchfix.com>
2018-12-17 11:57:19 -0500

    Add tests for arguments matcher and error messages.  Largely based on existing ActiveJob matcher tests.

--- spec/rspec/rails/matchers/have_enqueued_mail_spec.rb
+++ spec/rspec/rails/matchers/have_enqueued_mail_spec.rb
@@ -22,5 +22,68 @@ RSpec.describe "HaveEnqueuedMail matchers", skip: !RSpec::Rails::FeatureCheck.ha
         TestMailer.test_email.deliver_later
       }.to have_enqueued_mail(TestMailer, :test_email)
     end
+
+    it "passes when using the have_enqueued_email alias" do
+      expect {
+        TestMailer.test_email.deliver_later
+      }.to have_enqueued_email(TestMailer, :test_email)
+    end
+
+    it "passes when using the enqueue_mail alias" do
+      expect {
+        TestMailer.test_email.deliver_later
+      }.to enqueue_mail(TestMailer, :test_email)
+    end
+
+    it "passes when using the enqueue_email alias" do
+      expect {
+        TestMailer.test_email.deliver_later
+      }.to enqueue_email(TestMailer, :test_email)
+    end
+
+    it "passes when negated" do
+      expect { }.not_to have_enqueued_email(TestMailer, :test_email)
+    end
+
+    # it "counts only emails enqueued in the block" do
+      # TestMailer.test_email
+
+      # expect {
+        # TestMailer.test_email
+      # }.to have_enqueued_email(TestMailer, :test_email).once
+    # end
+
+    it "passes with provided argument matchers" do
+      expect {
+        TestMailer.email_with_args(1, 2).deliver_later
+      }.to have_enqueued_mail(TestMailer, :email_with_args).with(1, 2)
+
+      expect {
+        TestMailer.email_with_args(1, 2).deliver_later
+      }.not_to have_enqueued_mail(TestMailer, :email_with_args).with(3, 4)
+    end
+
+    it "generates a failure message" do
+      expect {
+        expect { }.to have_enqueued_email(TestMailer, :test_email)
+      }.to raise_error(/expected to enqueue TestMailer.test_email/)
+    end
+
+    it "generates a failure message with arguments" do
+      expect {
+        expect { }.to have_enqueued_email(TestMailer, :email_with_args).with(1, 2)
+      }.to raise_error(/expected to enqueue TestMailer.email_with_args with \[1, 2\]/)
+    end
+
+    it "throws descriptive error when no test adapter set" do
+      queue_adapter = ActiveJob::Base.queue_adapter
+      ActiveJob::Base.queue_adapter = :inline
+
+      expect {
+        expect { TestMailer.test_email }.to have_enqueued_mail(TestMailer, :test_email)
+      }.to raise_error("To use ActiveJob matchers set `ActiveJob::Base.queue_adapter = :test`")
+
+      ActiveJob::Base.queue_adapter = queue_adapter
+    end
   end
 end
