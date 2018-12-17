e2e3adfe0edfc13e9f697a2177474d9acef72fe0
OKURA Masafumi <masafumi.o1988@gmail.com>
2018-12-17 20:58:18 +0900

    Use __dir__ for expand_path in rails_helper template
    Ruby 2.0 introduced `__dir__` pseudo variable, and we can replace it with `__FILE__` for `expand_path` method.
    There is a RuboCop Cop pointing this out.
    http://www.rubocop.org/en/latest/cops_style/#styleexpandpatharguments
    This change reduces one RuboCop warning after installing rspec-rails.

--- lib/generators/rspec/install/templates/spec/rails_helper.rb
+++ lib/generators/rspec/install/templates/spec/rails_helper.rb
@@ -1,7 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
