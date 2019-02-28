5c7d74fe57269e25f43898d975ae07cb095d387d
Conor Souhrada <ConorSouhrada@gmail.com>
2019-02-27 19:52:32 -0700

    fixed issues causing failing tests

--- lib/generators/rspec/generators/generator_generator.rb
+++ lib/generators/rspec/generators/generator_generator.rb
@@ -3,7 +3,7 @@ require 'generators/rspec'
 module Rspec
   module Generators
     # @private
-    class GeneratorGenerator < Base
+    class GeneratorsGenerator < Base
       class_option :generator_specs, :type => :boolean, :default => true,  :desc => "Generate generator specs"
 
       def generate_generator_spec
--- spec/generators/rspec/generators/generator_generator_spec.rb
+++ spec/generators/rspec/generators/generator_generator_spec.rb
@@ -1,37 +1,37 @@
-  require 'generators/rspec/generator/generator_generator'
-  require 'support/generators'
+require 'generators/rspec/generators/generator_generator'
+require 'support/generators'
 
-  RSpec.describe Rspec::Generators::GeneratorGenerator, :type => :generator do
-    setup_default_destination
+ RSpec.describe Rspec::Generators::GeneratorsGenerator, :type => :generator do
+   setup_default_destination
 
-    describe "generator specs" do
-      subject(:generator_spec) { file("spec/generator/posts_generator_spec.rb") }
-      describe "are generated independently from the command line" do
-        before do
-          run_generator %w(posts)
-        end
-        describe "the spec" do
-          it "exists" do
-            expect(generator_spec).to exist
-          end
-          it "contains 'rails_helper'" do
-            expect(generator_spec).to contain(/require 'rails_helper'/)
-          end
-          it "contains the system" do
-            expect(generator_spec).to contain(/^RSpec.describe \"Posts\", #{type_metatag(:generator)}/)
-          end
-        end
-      end
+   describe "generator specs" do
+     subject(:generator_spec) { file("spec/generator/posts_generator_spec.rb") }
+     describe "are generated independently from the command line" do
+       before do
+         run_generator %w(posts)
+       end
+       describe "the spec" do
+         it "exists" do
+           expect(generator_spec).to exist
+         end
+         it "contains 'rails_helper'" do
+           expect(generator_spec).to contain(/require 'rails_helper'/)
+         end
+         it "contains the system" do
+           expect(generator_spec).to contain(/^RSpec.describe \"Posts\", #{type_metatag(:generator)}/)
+         end
+       end
+     end
 
-      describe "are not generated" do
-        before do
-          run_generator %w(posts --no-generator-specs)
-        end
-        describe "the spec" do
-          it "does not exist" do
-            expect(generator_spec).to_not exist
-          end
-        end
-      end
-    end
-  end
+     describe "are not generated" do
+       before do
+         run_generator %w(posts --no-generator-specs)
+       end
+       describe "the spec" do
+         it "does not exist" do
+           expect(generator_spec).to_not exist
+         end
+       end
+     end
+   end
+ end
