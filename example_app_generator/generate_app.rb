a400ee3cd6f9cb97d05f744db9aeb8ac7ddf41d0
Sam Phippen <samphippen@googlemail.com>
2019-04-20 11:11:53 +0900

    Fix various subbing in builds

--- example_app_generator/generate_app.rb
+++ example_app_generator/generate_app.rb
@@ -29,13 +29,14 @@ in_root do
     gsub_file "Gemfile", /.*rdoc.*/, "gem 'rdoc', '< 6'"
   end
 
+  if Rails::VERSION::STRING >= '5.0.0'
+    append_to_file('Gemfile', "gem 'rails-controller-testing'\n")
+  end
+
   if Rails::VERSION::STRING >= '6'
     gsub_file "Gemfile", /.*jbuilder.*/, "gem 'jbuilder', :git => 'https://github.com/rails/jbuilder.git', :branch => 'master'"
     gsub_file "Gemfile", /.*sqlite3.*/, "gem 'sqlite3', '~> 1.4'"
-  end
-
-  if Rails::VERSION::STRING >= '5.0.0'
-    append_to_file('Gemfile', "gem 'rails-controller-testing', :git => 'https://github.com/rails/rails-controller-testing'\n")
+    gsub_file "Gemfile", /.*rails-controller-testing.*/, "gem 'rails-controller-testing', git: 'https://github.com/rails/rails-controller-testing'"
   end
 
   if Rails::VERSION::STRING >= "5.1.0"
