e0d647023d5c70eb930eb9ef39907d755f7fd1dc
Sam Phippen <samphippen@googlemail.com>
2019-05-01 13:42:16 -0500

    rubbycop

--- lib/rspec/rails/example/view_example_group.rb
+++ lib/rspec/rails/example/view_example_group.rb
@@ -130,11 +130,10 @@ module RSpec
         def _default_render_options
           if ::Rails::VERSION::STRING >= '3.2'
             formats = if ActionView::Template::Types.respond_to?(:symbols)
-              ActionView::Template::Types.symbols
-            else
-              [:html, :text, :js, :css, :xml, :json].map(&:to_s)
+                        ActionView::Template::Types.symbols
+                      else
+                        [:html, :text, :js, :css, :xml, :json].map(&:to_s)
             end.map { |x| Regexp.escape(x) }.join("|")
-
 
             handlers = ActionView::Template::Handlers.extensions.map { |x| Regexp.escape(x) }.join("|")
             locales = "[a-z]{2}(?:-[A-Z]{2})?"
