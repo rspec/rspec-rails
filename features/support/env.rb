9aa7d72fa3ee163b4d15cd6502354a97c7578d01
Jon Rowe <hello@jonrowe.co.uk>
2019-12-09 13:03:59 +0000

    Merge pull request #2140 from mvz/update-aruba
    Update aruba to version 0.14.12

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,23 +2,21 @@ require 'aruba/cucumber'
 require 'fileutils'
 
 module ArubaExt
-  def run(cmd, timeout = nil)
+  def run_command(cmd, timeout = nil)
     exec_cmd = cmd =~ /^rspec/ ? "bin/#{cmd}" : cmd
-    super(exec_cmd, timeout)
-  end
-  # This method over rides Aruba 0.5.4 implementation so that we can reset Bundler to use the sample app Gemfile
-  def in_current_dir(&block)
-    Bundler.with_clean_env do
-      _mkdir(current_dir)
-      Dir.chdir(current_dir, &block)
+    # Ensure bundler env vars are unset
+    unset_bundler_env_vars
+    # Ensure the correct Gemfile is found
+    in_current_directory do
+      super(exec_cmd, timeout)
     end
   end
 end
 
 World(ArubaExt)
 
-Before do
-  @aruba_timeout_seconds = 30
+Aruba.configure do |config|
+  config.exit_timeout = 30
 end
 
 unless File.directory?('./tmp/example_app')
