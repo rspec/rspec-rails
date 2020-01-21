ca988b0697524717e41ce71663869708273b1966
Benoit Tigeot <benoit@hopsandfork.com>
2020-01-21 22:13:16 +0100

    Rails generate generator command create spec by default

--- spec/generators/rspec/generator/generator_generator_spec.rb
+++ spec/generators/rspec/generator/generator_generator_spec.rb
@@ -6,30 +6,17 @@ RSpec.describe Rspec::Generators::GeneratorGenerator, :type => :generator do
 
   describe "generator specs" do
     subject(:generator_spec) { file("spec/generator/posts_generator_spec.rb") }
-    describe "are generated independently/can be generated" do
-      before do
-        run_generator %w(posts --generator-specs)
-      end
-      it "creates the spec file" do
-        expect(generator_spec).to exist
-      end
-      it "contains 'rails_helper in the spec file'" do
-        expect(generator_spec).to contain(/require 'rails_helper'/)
-      end
-      it "includes the generator type in the metadata" do
-        expect(generator_spec).to contain(/^RSpec.describe \"Posts\", #{type_metatag(:generator)}/)
-      end
+    before do
+      run_generator %w(posts)
     end
-
-    describe "are not generated/are skipped by default" do
-      before do
-        run_generator %w(posts)
-      end
-      describe "the spec" do
-        it "does not exist" do
-          expect(generator_spec).to_not exist
-        end
-      end
+    it "creates the spec file by default" do
+      expect(generator_spec).to exist
+    end
+    it "contains 'rails_helper in the spec file'" do
+      expect(generator_spec).to contain(/require 'rails_helper'/)
+    end
+    it "includes the generator type in the metadata" do
+      expect(generator_spec).to contain(/^RSpec.describe \"Posts\", #{type_metatag(:generator)}/)
     end
   end
 end
