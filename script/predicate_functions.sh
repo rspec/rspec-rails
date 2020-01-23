3ef98eea2206500498a7e2f35721b0121c5f3e33
Phil Pirozhkov <hello@fili.pp.ru>
2020-01-23 20:48:55 +0300

    Updated travis build scripts (from rspec-dev)

--- script/predicate_functions.sh
+++ script/predicate_functions.sh
@@ -1,4 +1,4 @@
-# This file was generated on 2019-12-18T14:01:39+00:00 from the rspec-dev repo.
+# This file was generated on 2020-01-23T20:48:55+03:00 from the rspec-dev repo.
 # DO NOT modify it by hand as your changes will get lost the next time it is generated.
 
 function is_mri {
@@ -57,16 +57,28 @@ function is_mri_2plus {
   fi
 }
 
-function is_ruby_23_plus {
-  if ruby -e "exit(RUBY_VERSION.to_f >= 2.3)"; then
+function is_ruby_25_plus {
+  if ruby -e "exit(RUBY_VERSION.to_f >= 2.5)"; then
     return 0
   else
     return 1
   fi
 }
 
+function is_mri_27 {
+  if is_mri; then
+    if ruby -e "exit(RUBY_VERSION.to_f == 2.7)"; then
+      return 0
+    else
+      return 1
+    fi
+  else
+    return 1
+  fi
+}
+
 function rspec_rails_compatible {
-  if is_ruby_23_plus; then
+  if is_ruby_25_plus; then
     return 0
   else
     return 1
@@ -89,7 +101,11 @@ function additional_specs_available {
 function documentation_enforced {
   if [ -x ./bin/yard ]; then
     if is_mri_2plus; then
-      return 0
+      if is_mri_27; then
+        return 1
+      else
+        return 0
+      fi
     else
       return 1
     fi
