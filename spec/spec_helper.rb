03a3f823bca4f2e79fce124c97259fa2802411c7
Vyacheslav Bobrov <babrovka@gmail.com>
2020-02-17 16:51:59 +0300

    Style/RedundantSelf

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ require 'rails/all'
 
 module RSpecRails
   class Application < ::Rails::Application
-    self.config.secret_key_base = 'ASecretString'
+    config.secret_key_base = 'ASecretString'
 
     if defined?(ActionCable)
       ActionCable.server.config.cable = { "adapter" => "test" }
