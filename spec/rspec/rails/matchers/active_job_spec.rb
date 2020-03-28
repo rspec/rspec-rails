01c80c45280f4f22e415fd0d06a50f5ce97770bf
Jon Rowe <hello@jonrowe.co.uk>
2020-03-28 10:05:39 +0000

    Merge pull request #2304 from rspec/fix-enqueued-at-with-time
    Fix comparison of times for `#at` in job matchers.

--- spec/rspec/rails/matchers/active_job_spec.rb
+++ spec/rspec/rails/matchers/active_job_spec.rb
@@ -216,6 +216,13 @@ RSpec.describe "ActiveJob matchers", skip: !RSpec::Rails::FeatureCheck.has_activ
       }.to have_enqueued_job.at(date)
     end
 
+    it "passes with provided at time" do
+      time = Time.now + 1.day
+      expect {
+        hello_job.set(wait_until: time).perform_later
+      }.to have_enqueued_job.at(time)
+    end
+
     it "accepts composable matchers as an at date" do
       future = 1.minute.from_now
       slightly_earlier = 58.seconds.from_now
