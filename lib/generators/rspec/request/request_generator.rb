d1692bd00fb361b00c7b272a7ac0de57078d3599
Luka Lüdicke <klukasocial+github@gmail.com>
2020-09-25 19:08:20 +0200

    Push down code from IntegrationGenerator to RequestGenerator
    and break the inheritance.
    IntegrationGenerator will get a deprecation
    so the inheritance made no sense.

--- lib/generators/rspec/request/request_generator.rb
+++ lib/generators/rspec/request/request_generator.rb
@@ -1,10 +1,19 @@
-require 'generators/rspec/integration/integration_generator'
+require 'generators/rspec'
 
 module Rspec
   module Generators
     # @private
-    class RequestGenerator < IntegrationGenerator
+    class RequestGenerator < Base
+      class_option :request_specs, type: :boolean, default: true, desc: 'Generate request specs'
+
       source_paths << File.expand_path('../integration/templates', __dir__)
+
+      def generate_request_spec
+        return unless options[:request_specs]
+
+        template 'request_spec.rb',
+                 File.join('spec/requests', "#{name.underscore.pluralize}_spec.rb")
+      end
     end
   end
 end
