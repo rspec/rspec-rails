286057202d9bd86cd3bce1e49b1fe3ca0098b91b
Luka Lüdicke <klukasocial+github@gmail.com>
2020-10-11 11:18:40 +0200

    Add only one test for integration generator deprecation

--- spec/generators/rspec/integration/integration_generator_spec.rb
+++ spec/generators/rspec/integration/integration_generator_spec.rb
@@ -1,8 +1,14 @@
 # Generators are not automatically loaded by Rails
 require 'generators/rspec/integration/integration_generator'
 require 'support/generators'
+require 'rspec/core/warnings'
 
 RSpec.describe Rspec::Generators::IntegrationGenerator, type: :generator do
   setup_default_destination
   it_behaves_like "a request spec generator"
+
+  it 'is deprecated' do
+    expect(RSpec).to receive(:warn_deprecation)
+    run_generator %w[posts]
+  end
 end
