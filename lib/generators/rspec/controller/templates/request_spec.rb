8658247154dd402c49743e9f2c5fb89d5798a501
Phil Pirozhkov <pirj@users.noreply.github.com>
2020-10-31 23:41:08 +0300

    Merge pull request #2399 from ThHareau/feat/ticket-2375
    Improve controller template when no action

--- lib/generators/rspec/controller/templates/request_spec.rb
+++ lib/generators/rspec/controller/templates/request_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "<%= class_name.pluralize %>", <%= type_metatag(:request) %> do
-<% namespaced_path = regular_class_path.join('/') %>
+<% namespaced_path = regular_class_path.join('/') -%>
+<% if actions.empty? -%>
+  describe "GET /index" do
+    pending "add some examples (or delete) #{__FILE__}"
+  end
+<% end -%>
 <% for action in actions -%>
   describe "GET /<%= action %>" do
     it "returns http success" do
