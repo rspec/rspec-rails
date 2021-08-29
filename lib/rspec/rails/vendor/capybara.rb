25e150fdcd534c6b411a42cd22b192f7fefa2c66
YusukeIwaki <iwaki@i3-systems.com>
2021-08-30 01:47:37 +0900

    Remove redundunt includes for Capybara

--- lib/rspec/rails/vendor/capybara.rb
+++ lib/rspec/rails/vendor/capybara.rb
@@ -10,20 +10,18 @@ end
 
 if defined?(Capybara)
   RSpec.configure do |c|
-    if defined?(Capybara::DSL)
-      c.include Capybara::DSL, type: :feature
-      if defined?(ActionPack) && ActionPack::VERSION::STRING >= "5.1"
-        c.include Capybara::DSL, type: :system
-      end
-    end
+    # 'capybara/rspec' defines configurations below.
+    #
+    # config.include Capybara::DSL, type: :feature
+    # config.include Capybara::RSpecMatchers, type: :feature
+    # config.include Capybara::DSL, type: :system
+    # config.include Capybara::RSpecMatchers, type: :system
+    # config.include Capybara::RSpecMatchers, type: :view
 
     if defined?(Capybara::RSpecMatchers)
-      c.include Capybara::RSpecMatchers, type: :view
       c.include Capybara::RSpecMatchers, type: :helper
       c.include Capybara::RSpecMatchers, type: :mailer
       c.include Capybara::RSpecMatchers, type: :controller
-      c.include Capybara::RSpecMatchers, type: :feature
-      c.include Capybara::RSpecMatchers, type: :system
     end
 
     unless defined?(Capybara::RSpecMatchers) || defined?(Capybara::DSL)
