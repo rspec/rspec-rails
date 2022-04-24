7faa22c642644be585a4ec30dbf0ecdae1740479
Jon Rowe <hello@jonrowe.co.uk>
2022-04-24 09:17:03 +0100

    Version 5.1.2

--- lib/rspec/rails/version.rb
+++ lib/rspec/rails/version.rb
@@ -3,7 +3,7 @@ module RSpec
     # Version information for RSpec Rails.
     module Version
       # Current version of RSpec Rails, in semantic versioning format.
-      STRING = '5.1.1'
+      STRING = '5.1.2'
     end
   end
 end
