a4837e74e628f9247d8ec46b033d5bdc1fcf090d
nekketsuuu <nekketsuuu@users.noreply.github.com>
2022-05-02 00:28:34 +0900

    Remove instance variables in generated specs RSpec/InstanceVariable rule of rubocop-rspec gem prefers no instance variable usages in specs by default, at least v2.10.0

--- lib/generators/rspec/scaffold/templates/show_spec.rb
+++ lib/generators/rspec/scaffold/templates/show_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 <% output_attributes = attributes.reject{|attribute| [:datetime, :timestamp, :time, :date].index(attribute.type) } -%>
 RSpec.describe "<%= ns_table_name %>/show", <%= type_metatag(:view) %> do
   before(:each) do
-    @<%= ns_file_name %> = assign(:<%= ns_file_name %>, <%= class_name %>.create!(<%= '))' if output_attributes.empty? %>
+    assign(:<%= ns_file_name %>, <%= class_name %>.create!(<%= '))' if output_attributes.empty? %>
 <% output_attributes.each_with_index do |attribute, attribute_index| -%>
       <%= attribute.name %>: <%= value_for(attribute) %><%= attribute_index == output_attributes.length - 1 ? '' : ','%>
 <% end -%>
