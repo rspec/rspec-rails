bc5a3c07992ee33cb7fba9ef4ce134b55fab00eb
Jon Rowe <hello@jonrowe.co.uk>
2022-10-17 12:06:14 +0100

    Merge pull request #2625 from rspec/replacement-tagged-logger-support
    Implement tagged_logger by hand

--- spec/rspec/rails/example/rails_example_group_spec.rb
+++ spec/rspec/rails/example/rails_example_group_spec.rb
@@ -1,8 +1,7 @@
 module RSpec::Rails
   RSpec.describe RailsExampleGroup do
     if ::Rails::VERSION::MAJOR >= 7
-      it 'includes ActiveSupport::Testing::TaggedLogging' do
-        expect(described_class.include?(::ActiveSupport::Testing::TaggedLogging)).to eq(true)
+      it 'supports tagged_logger' do
         expect(described_class.private_instance_methods).to include(:tagged_logger)
       end
     end
