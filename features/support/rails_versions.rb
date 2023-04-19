591f4f04bb4dffb21192c28222720864104759c2
Jason Yates <jason@jasonyates.me>
2023-04-19 14:27:26 -0400

    Fixes issue with ActiveStorage::FixtureSet.blob erroring because ActiveStorage::FixtureSet.file_fixture_path is not set.

---
+++ features/support/rails_versions.rb
@@ -0,0 +1,15 @@
+def rails_version
+  string_version = ENV.fetch("RAILS_VERSION", "~> 7.0.0")
+  if string_version == "main" || string_version.nil?
+    Float::INFINITY
+  else
+    string_version[/\d[.-]\d/].tr('-', '.')
+  end
+end
+
+Before "@rails_post_7" do |scenario|
+  if rails_version.to_f < 7.0
+    warn "Skipping scenario #{scenario.name} on Rails v#{rails_version}"
+    skip_this_scenario
+  end
+end
