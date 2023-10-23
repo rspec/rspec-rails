3bb2588947282de599dca9e6841803a273c2c0ed
Jon Rowe <hello@jonrowe.co.uk>
2023-10-23 15:35:38 +0100

    Merge pull request #2694 from taketo1113/view-spec-with-model-name
    Fix variable name when generate scaffold with namespace & model-name

--- lib/generators/rspec/scaffold/templates/edit_spec.rb
+++ lib/generators/rspec/scaffold/templates/edit_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 <% output_attributes = attributes.reject{|attribute| [:datetime, :timestamp, :time, :date].index(attribute.type) } -%>
 RSpec.describe "<%= ns_table_name %>/edit", <%= type_metatag(:view) %> do
-  let(:<%= ns_file_name %>) {
+  let(:<%= singular_table_name %>) {
     <%= class_name %>.create!(<%= ')' if output_attributes.empty? %>
 <% output_attributes.each_with_index do |attribute, attribute_index| -%>
       <%= attribute.name %>: <%= attribute.default.inspect %><%= attribute_index == output_attributes.length - 1 ? '' : ','%>
@@ -11,13 +11,13 @@ RSpec.describe "<%= ns_table_name %>/edit", <%= type_metatag(:view) %> do
   }
 
   before(:each) do
-    assign(:<%= ns_file_name %>, <%= ns_file_name %>)
+    assign(:<%= singular_table_name %>, <%= singular_table_name %>)
   end
 
   it "renders the edit <%= ns_file_name %> form" do
     render
 
-    assert_select "form[action=?][method=?]", <%= ns_file_name %>_path(<%= ns_file_name %>), "post" do
+    assert_select "form[action=?][method=?]", <%= ns_file_name %>_path(<%= singular_table_name %>), "post" do
 <% for attribute in output_attributes -%>
       <%- name = attribute.respond_to?(:column_name) ? attribute.column_name : attribute.name %>
       assert_select "<%= attribute.input_type -%>[name=?]", "<%= ns_file_name %>[<%= name %>]"
