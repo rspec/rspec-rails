bede4b68ed7bba64c8bb35b82a444e4d823affd7
Jon Rowe <hello@jonrowe.co.uk>
2023-11-16 09:21:59 +0000

    Wrap tests in executor when configured or on Rails 7 by default

--- lib/rspec/rails/example/rails_example_group.rb
+++ lib/rspec/rails/example/rails_example_group.rb
@@ -13,6 +13,23 @@ module RSpec
       include RSpec::Rails::MinitestAssertionAdapter
       include RSpec::Rails::FixtureSupport
       include RSpec::Rails::TaggedLoggingAdapter if ::Rails::VERSION::MAJOR >= 7
+
+      included do |_other|
+        around do |example|
+          use_exectuor =
+            case ::Rails.configuration.active_support.executor_around_test_case
+            when nil then ::Rails::VERSION::MAJOR >= 7
+            when true then true
+            when false then false
+            end
+
+          if use_exectuor
+            ::Rails.application.executor.perform { example.call }
+          else
+            example.call
+          end
+        end
+      end
     end
   end
 end
