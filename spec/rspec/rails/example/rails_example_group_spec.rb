26b38df3336773d87dbd66dc561363d535bc3443
Jon Rowe <hello@jonrowe.co.uk>
2023-11-16 09:31:50 +0000

    Merge pull request #2711 from rspec/fix-leak-of-execution-context
    Fix leak of execution context between specs

--- spec/rspec/rails/example/rails_example_group_spec.rb
+++ spec/rspec/rails/example/rails_example_group_spec.rb
@@ -5,5 +5,32 @@ module RSpec::Rails
         expect(described_class.private_instance_methods).to include(:tagged_logger)
       end
     end
+
+    it 'does not leak context between example groups', if: ::Rails::VERSION::MAJOR >= 7 do
+      groups =
+        [
+          RSpec::Core::ExampleGroup.describe("A group") do
+            include RSpec::Rails::RailsExampleGroup
+            specify { expect(ActiveSupport::ExecutionContext.to_h).to eq({}) }
+          end,
+          RSpec::Core::ExampleGroup.describe("A controller group", type: :controller) do
+            specify do
+              Rails.error.set_context(foo: "bar")
+              expect(ActiveSupport::ExecutionContext.to_h).to eq(foo: "bar")
+            end
+          end,
+          RSpec::Core::ExampleGroup.describe("Another group") do
+            include RSpec::Rails::RailsExampleGroup
+            specify { expect(ActiveSupport::ExecutionContext.to_h).to eq({}) }
+          end
+        ]
+
+      results =
+        groups.map do |group|
+          group.run(failure_reporter) ? true : failure_reporter.exceptions
+        end
+
+      expect(results).to all be true
+    end
   end
 end
