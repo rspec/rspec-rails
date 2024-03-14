e0631380e35d0da6880d090dc3ee2d391609c230
Nicolas Buduroi <nbuduroi@gmail.com>
2024-03-14 11:27:11 -0400

    Clean up generator for generator specs
    Use singular name to match the generator name and use the generator name
    in the spec description.
    
    Also clean up an extra whitespace and empty line in the template.

--- lib/generators/rspec/generator/templates/generator_spec.rb
+++ lib/generators/rspec/generator/templates/generator_spec.rb
@@ -1,6 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe "<%= class_name.pluralize %>", <%= type_metatag(:generator) %> do
-
+RSpec.describe "<%= class_name %>Generator", <%= type_metatag(:generator) %> do
   pending "add some scenarios (or delete) #{__FILE__}"
 end
