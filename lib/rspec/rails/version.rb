916ea266ce9e6b73db5fab7d55b0b9738265ab38
Jon Rowe <hello@jonrowe.co.uk>
2024-03-18 20:46:01 +0000

    v6.1.2

--- lib/rspec/rails/version.rb
+++ lib/rspec/rails/version.rb
@@ -3,7 +3,7 @@ module RSpec
     # Version information for RSpec Rails.
     module Version
       # Current version of RSpec Rails, in semantic versioning format.
-      STRING = '6.1.1'
+      STRING = '6.1.2'
     end
   end
 end
