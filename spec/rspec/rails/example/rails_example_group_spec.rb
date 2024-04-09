91167a02a557e35f8a41a1702d82d8ec1751b5ea
Javier Julio <JJfutbol@gmail.com>
2024-04-09 10:56:12 -0400

    Add spec

--- spec/rspec/rails/example/rails_example_group_spec.rb
+++ spec/rspec/rails/example/rails_example_group_spec.rb
@@ -32,5 +32,40 @@ module RSpec::Rails
 
       expect(results).to all be true
     end
+
+    describe 'CurrentAttributes' do
+      let(:current_class) do 
+        Class.new(ActiveSupport::CurrentAttributes) do
+          attribute :request_id
+        end
+      end
+
+      it 'does not leak current attributes between example groups', if: ::Rails::VERSION::MAJOR >= 7 do
+        groups =
+          [
+            RSpec::Core::ExampleGroup.describe("A group") do
+              include RSpec::Rails::RailsExampleGroup
+              specify { expect(current_class.attributes).to eq({}) }
+            end,
+            RSpec::Core::ExampleGroup.describe("A controller group", type: :controller) do
+              specify do
+                current_class.request_id = "123"
+                expect(current_class.attributes).to eq(request_id: "123")
+              end
+            end,
+            RSpec::Core::ExampleGroup.describe("Another group") do
+              include RSpec::Rails::RailsExampleGroup
+              specify { expect(current_class.attributes).to eq({}) }
+            end
+          ]
+      
+        results =
+          groups.map do |group|
+            group.run(failure_reporter) ? true : failure_reporter.exceptions
+          end
+      
+        expect(results).to all be true
+      end
+    end
   end
 end
