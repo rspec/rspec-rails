28bca50ae8dab5ab4b014270631b427280d700c1
Jon Rowe <hello@jonrowe.co.uk>
2024-04-10 09:30:11 +0100

    Merge pull request #2752 from javierjulio/patch-1
    Reset CurrentAttributes on each rails example

--- spec/rspec/rails/example/rails_example_group_spec.rb
+++ spec/rspec/rails/example/rails_example_group_spec.rb
@@ -1,9 +1,7 @@
 module RSpec::Rails
   RSpec.describe RailsExampleGroup do
-    if ::Rails::VERSION::MAJOR >= 7
-      it 'supports tagged_logger' do
-        expect(described_class.private_instance_methods).to include(:tagged_logger)
-      end
+    it 'supports tagged_logger', if: ::Rails::VERSION::MAJOR >= 7 do
+      expect(described_class.private_instance_methods).to include(:tagged_logger)
     end
 
     it 'does not leak context between example groups', if: ::Rails::VERSION::MAJOR >= 7 do
@@ -31,6 +29,32 @@ module RSpec::Rails
         end
 
       expect(results).to all be true
+    end
+
+    it 'will not leak ActiveSupport::CurrentAttributes between examples', if: ::Rails::VERSION::MAJOR >= 7 do
+      group =
+        RSpec::Core::ExampleGroup.describe("A group", order: :defined) do
+          include RSpec::Rails::RailsExampleGroup
+
+          # rubocop:disable Lint/ConstantDefinitionInBlock
+          class CurrentSample < ActiveSupport::CurrentAttributes
+            attribute :request_id
+          end
+          # rubocop:enable Lint/ConstantDefinitionInBlock
+
+          it 'sets a current attribute' do
+            CurrentSample.request_id = '123'
+            expect(CurrentSample.request_id).to eq('123')
+          end
+
+          it 'does not leak current attributes' do
+            expect(CurrentSample.request_id).to eq(nil)
+          end
+        end
+
+      expect(
+        group.run(failure_reporter) ? true : failure_reporter.exceptions
+      ).to be true
     end
   end
 end
