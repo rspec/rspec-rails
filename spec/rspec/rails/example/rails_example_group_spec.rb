c35bb77744a65a2991996137c9a722b8b6751df1
Andrew Hodgkinson <andrew@ripaglobal.com>
2024-07-04 18:31:43 +1200

    Use 'in_sub_process' to properly manage global state mutation

--- spec/rspec/rails/example/rails_example_group_spec.rb
+++ spec/rspec/rails/example/rails_example_group_spec.rb
@@ -1,3 +1,5 @@
+require 'rspec/support/spec/in_sub_process'
+
 module RSpec::Rails
   RSpec.describe RailsExampleGroup do
     it 'supports tagged_logger', if: ::Rails::VERSION::MAJOR >= 7 do
@@ -68,13 +70,10 @@ module RSpec::Rails
       end
       # rubocop:enable Lint/ConstantDefinitionInBlock
 
-      # We have to modify the suite's around-each in RSpec.config, but don't
-      # want to pollute other tests with this (whether or not it is harmless
-      # to do so). There being no public API to read or remove hooks, instead
-      # it's necessary use some private APIs to be able to delete the added
-      # hook via 'ensure'.
+      # This dirties global state, so tests *MUST* remember to use
+      # "in_sub_process".
       #
-      around :each do | outer_example |
+      before :each do
 
         # Client code might legitimately want to wrap examples to ensure
         # all-conditions tidy-up, e.g. "ActsAsTenant.without_tenant do...",
@@ -84,58 +83,55 @@ module RSpec::Rails
         # time their actual tests, or their test hooks ran.
         #
         RSpec.configure do | config |
-          config.around(:each, uniquely_identifiable_metadata) do | inner_example |
+          config.around(:each, uniquely_identifiable_metadata) do | example |
             CurrentAttrsBetweenHooks.request_id = '123'
-            inner_example.run
+            example.run
           end
         end
-
-        outer_example.run
-
-      ensure
-        around_example_repository = RSpec.configuration.hooks.send(:hooks_for, :around, :example)
-        item_we_added = around_example_repository.items_for(uniquely_identifiable_metadata).first
-        around_example_repository.delete(item_we_added, uniquely_identifiable_metadata)
       end
 
       it 'does not reset ActiveSupport::CurrentAttributes before examples' do
-        group =
-          RSpec::Core::ExampleGroup.describe('A group', uniquely_identifiable_metadata) do
-            include RSpec::Rails::RailsExampleGroup
+        in_sub_process do
+          group =
+            RSpec::Core::ExampleGroup.describe('A group', uniquely_identifiable_metadata) do
+              include RSpec::Rails::RailsExampleGroup
 
-            it 'runs normally' do
-              expect(CurrentAttrsBetweenHooks.request_id).to eq('123')
+              it 'runs normally' do
+                expect(CurrentAttrsBetweenHooks.request_id).to eq('123')
+              end
             end
-          end
 
-        expect(
-          group.run(failure_reporter) ? true : failure_reporter.exceptions
-        ).to be true
+          expect(
+            group.run(failure_reporter) ? true : failure_reporter.exceptions
+          ).to be true
+        end
       end
 
       it 'does not reset ActiveSupport::CurrentAttributes before before-each hooks' do
-        group =
-          RSpec::Core::ExampleGroup.describe('A group', uniquely_identifiable_metadata) do
-            include RSpec::Rails::RailsExampleGroup
+        in_sub_process do
+          group =
+            RSpec::Core::ExampleGroup.describe('A group', uniquely_identifiable_metadata) do
+              include RSpec::Rails::RailsExampleGroup
 
-            # Client code will often have test setup blocks within "*_spec.rb"
-            # files that might set up data or other environmental factors for a
-            # group of tests in e.g. a "before" hook, but would reasonably expect
-            # suite-wide 'around' settings to remain intact and not be reset.
-            #
-            before :each do
-              expect(CurrentAttrsBetweenHooks.request_id).to eq('123')
-              CurrentAttrsBetweenHooks.request_id = '234'
+              # Client code will often have test setup blocks within "*_spec.rb"
+              # files that might set up data or other environmental factors for a
+              # group of tests in e.g. a "before" hook, but would reasonably expect
+              # suite-wide 'around' settings to remain intact and not be reset.
+              #
+              before :each do
+                expect(CurrentAttrsBetweenHooks.request_id).to eq('123')
+                CurrentAttrsBetweenHooks.request_id = '234'
+              end
+
+              it 'runs normally' do
+                expect(CurrentAttrsBetweenHooks.request_id).to eq('234')
+              end
             end
 
-            it 'runs normally' do
-              expect(CurrentAttrsBetweenHooks.request_id).to eq('234')
-            end
-          end
-
-        expect(
-          group.run(failure_reporter) ? true : failure_reporter.exceptions
-        ).to be true
+          expect(
+            group.run(failure_reporter) ? true : failure_reporter.exceptions
+          ).to be true
+        end
       end
     end
   end
