f3b81e29cdd05475047f33cb002715c24d5f74be
Masato Ohba <over.rye@gmail.com>
2025-02-06 17:40:20 +0900

    Update comment about `ActiveRecord::Migration.maintain_test_schema!` (#2832)
    The original comment implies that `maintain_test_schema!` applies pending migrations, but in reality, it purges and recreates the database, then loads the schema.

--- lib/generators/rspec/install/templates/spec/rails_helper.rb
+++ lib/generators/rspec/install/templates/spec/rails_helper.rb
@@ -28,7 +28,9 @@ require 'rspec/rails'
 # Rails.root.glob('spec/support/**/*.rb').sort_by(&:to_s).each { |f| require f }
 
 <% if RSpec::Rails::FeatureCheck.has_active_record_migration? -%>
-# Checks for pending migrations and applies them before tests are run.
+# Ensures that the test database schema matches the current schema file.
+# If there are pending migrations it will invoke `db:test:prepare` to
+# recreate the test database by loading the schema.
 # If you are not using ActiveRecord, you can remove these lines.
 begin
   ActiveRecord::Migration.maintain_test_schema!
