8b6cada8a40009386f3cb08848890d7f59c89534
Jon Rowe <hello@jonrowe.co.uk>
2026-02-09 13:20:06 +0000

    Remove invocation of --path

--- example_app_generator/ci_retry_bundle_install.sh
+++ example_app_generator/ci_retry_bundle_install.sh
@@ -4,4 +4,4 @@ set -e
 source FUNCTIONS_SCRIPT_FILE
 
 echo "Starting bundle install using shared bundle path"
-ci_retry eval "RUBYOPT=$RUBYOPT:' --enable rubygems' bundle install --gemfile ./Gemfile --path REPLACE_BUNDLE_PATH --retry=3 --jobs=3"
+ci_retry eval "bundle config set path REPLACE_BUNDLE_PATH; bundle install --gemfile ./Gemfile --retry=3 --jobs=3"
