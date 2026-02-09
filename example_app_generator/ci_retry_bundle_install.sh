01ac0899763f7ad227a4147d9efa41ec188f133e
Marvin Tangpos <marvin@codehit.net>
2026-02-09 21:01:49 +0800

    fix removed --binstubs, --standalone, and --path flags for Bundler 4.0 compatibility

--- example_app_generator/ci_retry_bundle_install.sh
+++ example_app_generator/ci_retry_bundle_install.sh
@@ -4,4 +4,5 @@ set -e
 source FUNCTIONS_SCRIPT_FILE
 
 echo "Starting bundle install using shared bundle path"
-ci_retry eval "RUBYOPT=$RUBYOPT:' --enable rubygems' bundle install --gemfile ./Gemfile --path REPLACE_BUNDLE_PATH --retry=3 --jobs=3"
+RUBYOPT="$RUBYOPT --enable rubygems" bundle config set --local path REPLACE_BUNDLE_PATH
+ci_retry eval "RUBYOPT=$RUBYOPT:' --enable rubygems' bundle install --gemfile ./Gemfile --retry=3 --jobs=3"
