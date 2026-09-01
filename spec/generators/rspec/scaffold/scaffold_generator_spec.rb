813a20406b8a289dfe19325cdfe6526680caa299
Jon Rowe <hello@jonrowe.co.uk>
2026-09-01 10:13:16 +0000

    Apply suggestion from @JonRowe

--- spec/generators/rspec/scaffold/scaffold_generator_spec.rb
+++ spec/generators/rspec/scaffold/scaffold_generator_spec.rb
@@ -76,10 +76,7 @@ RSpec.describe Rspec::Generators::ScaffoldGenerator, type: :generator do
     describe 'in an engine' do
       it 'generates files with Engine url_helpers' do
         in_sub_process do
-          # Stubbing defines the method on the class, and Thor registers every
-          # method defined on a generator as a command to run. `no_commands`
-          # keeps `mountable_engine?` out of the command list, which it is not
-          # a member of in the first place.
+          # Ensure Thor does not treat the stub as a command.
           ::Rails::Generators::NamedBase.no_commands do
             allow_any_instance_of(::Rails::Generators::NamedBase).to receive(:mountable_engine?).and_return(true)
           end
